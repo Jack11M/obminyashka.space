8af501f5a60e5f5586f399e034872f7d37b3a81b
Roman Kyrychenko <roman.kyrychenko@cengage.com>
2019-12-02 19:26:48 +0200

    CGE-40: Removed unnecessary test runner.

--- src/test/java/com/hillel/items_exchange/controller/AdvertisementControllerIntegrationTest.java
+++ src/test/java/com/hillel/items_exchange/controller/AdvertisementControllerIntegrationTest.java
@@ -9,12 +9,10 @@ import com.hillel.items_exchange.dto.*;
 import com.hillel.items_exchange.model.DealType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.transaction.Transactional;
@@ -27,7 +25,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @DBRider
 @AutoConfigureMockMvc
