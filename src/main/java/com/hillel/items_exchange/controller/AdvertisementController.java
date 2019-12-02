ecc69e449cb41c86dcba26c6f71cccc08e7e4028
Roman Kyrychenko <rp.kyrychenko@gmail.com>
2019-12-03 00:35:17 +0200

    CGE-40: Added CORS access

--- src/main/java/com/hillel/items_exchange/controller/AdvertisementController.java
+++ src/main/java/com/hillel/items_exchange/controller/AdvertisementController.java
@@ -5,14 +5,14 @@ import com.hillel.items_exchange.service.AdvertisementService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/adv")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 public class AdvertisementController {
 
