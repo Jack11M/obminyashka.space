c47980a607ea4998ee944d157cc26a9dd9abe8f8
Roman Kyrychenko <rp.kyrychenko@gmail.com>
2019-12-30 16:56:35 +0200

    CGE-40: Fixed populate of @CreatedDate and @LastModifiedDate annotations values.

--- src/main/java/com/hillel/items_exchange/config/AppConfig.java
+++ src/main/java/com/hillel/items_exchange/config/AppConfig.java
@@ -3,8 +3,10 @@ package com.hillel.items_exchange.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
+@EnableJpaAuditing(dateTimeProviderRef = "dateTimeProvider")
 public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
