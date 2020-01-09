59ec84c61f2d8073148b77eadc91dd24f18c72e2
Yevhenii Petrov <evgeniy.jyk@gmail.com>
2020-01-10 00:39:31 +0200

    CGE-45: Added unique constraint to the field name in Category

--- src/main/java/com/hillel/items_exchange/model/Category.java
+++ src/main/java/com/hillel/items_exchange/model/Category.java
@@ -16,6 +16,7 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(unique = true)
     private String name;
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
     private List<Subcategory> subcategories;
