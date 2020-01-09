465e91e10515ab3a349b361495e89e963d21528a
Yevhenii Petrov <evgeniy.jyk@gmail.com>
2020-01-10 00:35:23 +0200

    CGE-45: Renamed StringUtils into ExceptionTextMessage

---
+++ src/main/java/com/hillel/items_exchange/util/ExceptionTextMessage.java
@@ -0,0 +1,18 @@
+package com.hillel.items_exchange.util;
+
+public class ExceptionTextMessage {
+
+    public static final String NO_CATEGORY_BY_ID = "There is no category by id: ";
+    private static final String CATEGORY_CAN_NOT = "This category can not be ";
+    public static final String CAN_NOT_BE_UPDATED = CATEGORY_CAN_NOT + "updated: ";
+    public static final String MUST_HAVE_ID_ZERO = "New category or subcategory mustn't contain id except 0, " +
+            "also category name mustn't have duplicates!";
+    public static final String SUBCATEGORIES_MUST_EXIST_BY_ID_OR_ZERO = "The updated category and subcategories have " +
+            "to exist by id, new subcategories have to be 0 and also category name mustn't have duplicates except current name!";
+    public static final String CATEGORY_CAN_NOT_BE_DELETED = "The category can not be deleted by this id, " +
+            "because it has to exist by id and it's subcategories mustn't have products! Given category id: ";
+    public static final String SUBCATEGORY_CAN_NOT_BE_DELETED = "The subcategory can not be deleted by this id," +
+            " because it has to exist by id and mustn't have products! Given subcategory id: ";
+    public static final String HAS_ROLE_ADMIN = "hasRole('ROLE_ADMIN')";
+    public static final String SQL_EXCEPTION = "Exception during saving object to the database!\nError message:\n";
+}
