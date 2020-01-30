c71d5a26b7560248e877e02aa8ae9cf82bbad8ab
Roman Kyrychenko <roman.kyrychenko@cengage.com>
2020-01-30 14:43:09 +0200

    Updated project's structure for pure JavaScript front

--- front/src/main/js/src/app/_models/card.ts
+++
@@ -1,7 +0,0 @@
-export interface ICard {
-    id: number;
-    isFavourite: boolean;
-    img: string;
-    title: string;
-    location: string;
-}
--- front/src/main/js/src/app/_models/subcategory.ts
+++
@@ -1,4 +0,0 @@
-export interface ISubcategory {
-    id: number;
-    name: string;
-}
