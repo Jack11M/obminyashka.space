867a6096fb58425d3de6dc3698d0ea8f9130eef0
Yevhenii Petrov <evgeniy.jyk@gmail.com>
2020-02-03 01:04:13 +0200

    CGE-45: Added integration tests for CategoryController (positive cases) and create_category, update_category yml files

---
+++ src/test/java/com/hillel/items_exchange/controller/CategoryControllerIntegrationTest.java
@@ -0,0 +1,147 @@
+package com.hillel.items_exchange.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
+import com.github.database.rider.spring.api.DBRider;
+import com.hillel.items_exchange.dao.CategoryRepository;
+import com.hillel.items_exchange.dto.CategoryVo;
+import com.hillel.items_exchange.dto.SubcategoryVo;
+import com.hillel.items_exchange.service.SubcategoryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.transaction.Transactional;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@DBRider
+@AutoConfigureMockMvc
+public class CategoryControllerIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private CategoryRepository categoryRepository;
+    @Autowired
+    private SubcategoryService subcategoryService;
+    private CategoryVo existCategoryVo;
+
+    @BeforeEach
+    void setUp() {
+        createExistCategoryVo();
+    }
+
+    @Test
+    @Transactional
+    @DataSet("database_init.yml")
+    void getAllCategoriesNames_shouldReturnAllCategoriesNames() throws Exception {
+        mockMvc.perform(get("/category/names")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @Transactional
+    @DataSet("database_init.yml")
+    void getAllCategories_shouldReturnAllCategories() throws Exception {
+        mockMvc.perform(get("/category/all")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @Transactional
+    @DataSet("database_init.yml")
+    void getCategoryById_shouldReturnCategoryByIdIfExists() throws Exception {
+        mockMvc.perform(get("/category/{category_id}", 1L)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.name").value("shoes"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @Transactional
+    @DataSet("database_init.yml")
+    @ExpectedDataSet(value = "category/create_category.yml")
+    void createCategory_shouldCreateValidCategory() throws Exception {
+        CategoryVo nonExistCategoryVo = createNonExistCategoryVo();
+        mockMvc.perform(post("/category")
+                .content(asJsonString(nonExistCategoryVo))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").exists());
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @Transactional
+    @DataSet("database_init.yml")
+    @ExpectedDataSet(value = "category/update_category.yml")
+    void updateCategory_shouldUpdateExistedCategory() throws Exception {
+        existCategoryVo.setSubcategories(Arrays.asList(new SubcategoryVo(1L, "light shoes"),
+                new SubcategoryVo(0L, "winter shoes")));
+        existCategoryVo.setName("footwear");
+        mockMvc.perform(put("/category")
+                .content(asJsonString(existCategoryVo))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isAccepted())
+                .andExpect(jsonPath("$.name").value("footwear"))
+                .andExpect(jsonPath("$.subcategories", hasSize(2)));
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @Transactional
+    @DataSet("database_init.yml")
+    void deleteCategoryById_shouldDeleteExistedCategory() throws Exception {
+        mockMvc.perform(delete("/category/{category_id}", 2L)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").doesNotExist());
+    }
+
+    private void createExistCategoryVo() {
+        SubcategoryVo lightShoes = new SubcategoryVo(1L, "light_shoes");
+        existCategoryVo = new CategoryVo(1L, "shoes", Collections.singletonList(lightShoes));
+    }
+
+    private CategoryVo createNonExistCategoryVo() {
+        SubcategoryVo dress = new SubcategoryVo(0L, "dress");
+        SubcategoryVo trousers = new SubcategoryVo(0L, "trousers");
+        return new CategoryVo(0L, "clothes", Arrays.asList(dress, trousers));
+    }
+
+    private String asJsonString(final Object obj) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());
+            return objectMapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
