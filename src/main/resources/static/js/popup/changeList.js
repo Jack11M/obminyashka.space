b099e9eb934f1e84d2630ebbc2447464c0af79a2
Andrey Teslenko <andteslenko@gmail.com>
2020-02-19 11:28:27 +0200

    changed to a new design

--- src/main/resources/static/js/popup/changeList.js
+++ src/main/resources/static/js/popup/changeList.js
@@ -1,36 +1,29 @@
 function changeList() {
-  let arrayList = document.querySelectorAll(".tabs li");
-  let login = document.querySelector(".login");
-  let register = document.querySelector(".register");
-  let arrayId = [login, register];
+	let arrayList = document.querySelectorAll(".tabs li");
+	let login = document.querySelector(".login");
+	let register = document.querySelector(".register");
+	let arrayId = [login, register];
 
-  for (let key of arrayList) {
-    key.addEventListener("click", function() {
-      if (this.className !== "active") {
-        toggle();
-        isEmptyInput();
-      }
-    });
-  }
+	for (let key of arrayList) {
+		key.addEventListener("click", function () {
+			if (this.className !== "active") {
+				toggle();
+				new IsEmptyInputs().clearInputs();
+			}
+		});
+	}
 
-  function toggle() {
-    for (let key of arrayList) {
-      if (key.className !== "active") key.className = "active";
-      else key.classList.remove("active");
-    }
+	function toggle() {
+		for (let key of arrayList) {
+			if (key.className !== "active") key.className = "active";
+			else key.classList.remove("active");
+		}
 
-    for (let key of arrayId) {
-      key.classList.toggle("switch");
-    }
-  }
+		for (let key of arrayId) {
+			key.classList.toggle("switch");
+		}
+	}
 
-  function isEmptyInput() {
-    const emptyInput = document.querySelectorAll(".auth input");
-    emptyInput.forEach(input => {
-      if (input.id !== "check") {
-        input.value = "";
-      }
-    });
-  }
 }
+
 changeList();
