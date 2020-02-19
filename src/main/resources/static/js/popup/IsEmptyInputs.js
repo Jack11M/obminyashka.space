5685f55b79dcf9947acb49b0212b81f4f28b7ecc
Andrey Teslenko <andteslenko@gmail.com>
2020-02-19 11:47:34 +0200

    Done

---
+++ src/main/resources/static/js/popup/IsEmptyInputs.js
@@ -0,0 +1,16 @@
+class IsEmptyInputs {
+	constructor() {
+		this.emptyInput = document.querySelectorAll(".auth input");
+	}
+
+	clearInputs() {
+		ShowErrorsOfInputs.errors = [];
+		this.emptyInput.forEach(input => {
+			if (input.id !== "check" && input.id !== "check-reg") {
+				input.value = "";
+				ShowErrorsOfInputs.showSuccess(input);
+			}
+		});
+		ShowErrorsOfInputs.clearErrorStatus();
+	}
+}
