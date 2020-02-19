ee5039604b10b32c0ba8e7a91be0c3a1db797ccc
Andrey Teslenko <andteslenko@gmail.com>
2020-02-19 14:24:11 +0200

    Created the right branch for jira

---
+++ src/main/resources/static/js/popup/ConnectBase.js
@@ -0,0 +1,29 @@
+class ConnectBase {
+	constructor() {
+		this._url = "http://54.37.125.180:8080/auth/";
+		this.headers = {"Content-Type": "application/json"};
+	}
+
+	async postRequest(body, value) {
+		if (value.classList[1] === "btn-login") {
+			this._url += "login";
+		} else {
+			this._url += "register";
+		}
+		const response = await fetch(this._url, {
+			method: "POST",
+			headers: this.headers,
+			body: JSON.stringify(body)
+		}).catch(e => {
+			console.log(e);
+			alert("Нет подключения к базе");
+		});
+		try {
+			if (response) {
+				return await response.json();
+			}
+		} catch (e) {
+			alert("Что-то пошло не так...");
+		}
+	}
+}
---
+++ src/main/resources/static/js/popup/validation.js
@@ -0,0 +1,120 @@
+function RegisterValidation(data) {
+	const formEl = document.getElementById(data.id);
+	const formField = formEl.elements;
+
+	const validatorMethods = {
+		minLengthString: 1,
+		maxLengthString: 49,
+		rulesPattern: {
+			"email-reg": /.+@.+\..+/i,
+			"password-reg": /(?=^.{8,30}$)((?=.*\d)|(?=.*\W+))(?=.*[A-Z])(?=.*[a-z]).*$/
+		},
+
+		notEmpty(el) {
+			return el.value !== "";
+		},
+		pattern(el, pattern) {
+			return this.rulesPattern[pattern].test(el.value);
+		},
+		contains(el, el2) {
+			if (el.value === formField[el2].value) {
+				formField[el2].nextElementSibling.innerHTML = "";
+				return true;
+			} else return false;
+		},
+		lengthLogin(el) {
+			return (
+				el.value.length > this.minLengthString &&
+				el.value.length < this.maxLengthString &&
+				!el.value.includes(" ")
+			);
+		},
+	};
+
+	const isValid = function (el) {
+		let methods = data.methods[el.getAttribute("id")];
+		if (methods !== undefined) {
+			for (let key of methods) {
+				if (!validatorMethods[key[0]](el, key[1])) {
+					ShowErrorsOfInputs.errors.push({el});
+					return false;
+				}
+			}
+		}
+		return true;
+	};
+
+	const checkIt = function () {
+		if (isValid(this)) {
+			ShowErrorsOfInputs.showSuccess(this);
+			for (let i = 0; i < ShowErrorsOfInputs.errors.length; i++) {
+				if (ShowErrorsOfInputs.errors[i].el === this) {
+					ShowErrorsOfInputs.errors.splice(i, 1);
+				}
+			}
+		} else {
+			ShowErrorsOfInputs.showError(this);
+			let error = ShowErrorsOfInputs.errors.find(err => err.el === this);
+			if (!error) {
+				ShowErrorsOfInputs.errors.push({el: this});
+			}
+		}
+	};
+
+	const checkEmptyInput = function () {
+		for (let input of formField) {
+			if (input.tagName !== "BUTTON" && input.id !== "check" && input.id !== "check-reg") {
+				checkIt.call(input);
+			}
+		}
+	};
+
+	const createBody = function () {
+		const formData = new FormData(formEl);
+		const body = {};
+		for (let key of formData) {
+			const [name, value] = key;
+			body[name] = value;
+		}
+		return body;
+	};
+
+	for (let val of formField) {
+		if (val.id !== "check" && val.tagName !== "BUTTON" && val.id !== "check-reg") {
+			val.addEventListener("change", checkIt);
+		}
+	}
+
+	for (let val of formField) {
+		if (val.tagName === "BUTTON") {
+			val.addEventListener("click", async function (event) {
+				event.preventDefault();
+				checkEmptyInput();
+				if (ShowErrorsOfInputs.errors.length === 0) {
+					const connect = new ConnectBase();
+					const response = await connect.postRequest(createBody(), val);
+					ShowErrorsOfInputs.showErrorRegisterOrLogin(response);
+					console.log(response);
+				}
+			});
+		}
+	}
+}
+
+const validFormRegistration = new RegisterValidation({
+	id: "register-form",
+	methods: {
+		"email-reg": [["notEmpty"], ["pattern", "email-reg"]],
+		"login-reg": [["notEmpty"], ["lengthLogin"]],
+		"password-reg": [["notEmpty"], ["pattern", "password-reg"]],
+		"confirm-password": [["notEmpty"], ["contains", "password-reg"]]
+	}
+});
+
+const validFormLogin = new RegisterValidation({
+	id: "login-form",
+	methods: {
+		email: [["notEmpty"]],
+		password: [["notEmpty"]]
+	}
+});
