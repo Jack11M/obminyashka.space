3e7f7ea99d79e97113bd2b41c42963e2c8384143
LebedevMichael <LebedevMichael1990@gmail.com>
2020-03-05 23:02:54 +0200

    CGE-64 Added redirects to pages create.html, favorites.html, info.html, profile.html, registration.html, result.html, settings.html

--- src/main/java/com/hillel/items_exchange/controller/PageController.java
+++ src/main/java/com/hillel/items_exchange/controller/PageController.java
@@ -2,11 +2,48 @@ package com.hillel.items_exchange.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PageController {
     @GetMapping("/temp")
     public String getTemp() {
         return "temp";
+    }
+
+    @GetMapping("/registration")
+    public String getRegistration() {
+        return "registration";
+    }
+
+    @GetMapping("/search")
+    public String getResult() {
+        return "result";
+    }
+
+    @GetMapping("/product/create")
+    public String getCreate() {
+        return "create";
+    }
+
+    @GetMapping("/product/info")
+    public String getInfo() {
+        return "info";
+    }
+
+    @GetMapping("/profile")
+    public String getProfile() {
+        return "profile";
+    }
+
+    @GetMapping("/profile/favorites")
+    public String getFavorites() {
+        System.out.println("start method getFavorites");
+        return "favorites";
+    }
+
+    @GetMapping("/profile/settings")
+    public String getSettings() {
+        return "settings";
     }
 }
