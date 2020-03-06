a306ac6b2131d832f7d31f09e6822985917d83b1
Yevhenii Petrov <evgeniy.jyk@gmail.com>
2020-03-06 19:13:44 +0200

    CGE-45: Fixed Category Controller integration tests

--- src/test/java/com/hillel/items_exchange/controller/CategoryControllerIntegrationTest.java
+++ src/test/java/com/hillel/items_exchange/controller/CategoryControllerIntegrationTest.java
@@ -4,8 +4,8 @@ import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.core.api.dataset.ExpectedDataSet;
 import com.github.database.rider.spring.api.DBRider;
 import com.hillel.items_exchange.dto.CategoryDto;
-import com.hillel.items_exchange.dto.SubcategoryDto;
 import com.hillel.items_exchange.exception.InvalidDtoException;
+import com.hillel.items_exchange.util.CategoryControllerIntegrationTestUtil;
 import org.hibernate.boot.model.naming.IllegalIdentifierException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,25 +19,30 @@ import org.springframework.test.web.servlet.MvcResult;
 
 import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Optional;
 
 import static com.hillel.items_exchange.util.TestUtil.asJsonString;
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @DBRider
 @AutoConfigureMockMvc
 @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:index-reset.sql")
-public class CategoryControllerIntegrationTest {
+public class CategoryControllerIntegrationTest extends CategoryControllerIntegrationTestUtil {
+
+    private final MockMvc mockMvc;
+
     @Autowired
-    private MockMvc mockMvc;
+    public CategoryControllerIntegrationTest(MockMvc mockMvc) {
+        super();
+        this.mockMvc = mockMvc;
+    }
 
     @Test
     @Transactional
@@ -63,7 +68,7 @@ public class CategoryControllerIntegrationTest {
     @Transactional
     @DataSet("database_init.yml")
     void getCategoryById_shouldReturnCategoryByIdIfExists() throws Exception {
-        mockMvc.perform(get("/category/{category_id}", 1L)
+        mockMvc.perform(get("/category/{category_id}", EXISTING_ENTITY_ID)
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(jsonPath("$.id").value(1))
@@ -73,24 +78,20 @@ public class CategoryControllerIntegrationTest {
 
     @Test
     @DataSet("database_init.yml")
-    void getCategoryById_whenCategoryIdDoesNotExist_shouldReturnNotFoundAndThrowEntityNotFoundException() throws Exception {
-        long nonExistentCategoryId = 100000000L;
+    void getCategoryById_whenCategoryIdDoesNotExist_shouldReturnNotFoundAndThrowEntityNotFoundException()
+            throws Exception {
 
-        MvcResult result = this.mockMvc.perform(get("/category/{category_id}", nonExistentCategoryId)
+        MvcResult result = this.mockMvc.perform(get("/category/{category_id}", NONEXISTENT_ENTITY_ID)
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isNotFound())
                 .andReturn();
 
-        Optional<EntityNotFoundException> entityNotFoundException =
-                Optional.ofNullable((EntityNotFoundException) result.getResolvedException());
-
-        entityNotFoundException.ifPresent((e) -> assertThat(e, is(notNullValue())));
-        entityNotFoundException.ifPresent((e) -> assertThat(e, is(instanceOf(EntityNotFoundException.class))));
+        assertThat(result.getResolvedException(), is(instanceOf(EntityNotFoundException.class)));
     }
 
     @Test
-    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @WithMockUser(username = USERNAME_ADMIN, roles = {ROLE_ADMIN})
     @Transactional
     @DataSet("database_init.yml")
     @ExpectedDataSet(value = "category/create_category.yml")
@@ -118,14 +119,13 @@ public class CategoryControllerIntegrationTest {
     }
 
     @Test
-    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @WithMockUser(username = USERNAME_ADMIN, roles = {ROLE_ADMIN})
     @Transactional
     @DataSet("database_init.yml")
     void createCategory_whenCategoryIdNotEqualsZero_shouldReturnBadRequestAndThrowInvalidDtoException()
             throws Exception {
+
         CategoryDto nonExistCategoryDtoWithInvalidId = createNonExistCategoryDtoWithInvalidId();
-        String errorMessage = "New category or subcategory mustn't contain id except 0, " +
-                "also category name mustn't have duplicates!";
 
         MvcResult result = mockMvc.perform(post("/category")
                 .content(asJsonString(nonExistCategoryDtoWithInvalidId))
@@ -133,22 +133,18 @@ public class CategoryControllerIntegrationTest {
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isBadRequest())
-                .andExpect(content().string(containsString(errorMessage)))
                 .andReturn();
 
-        Optional<InvalidDtoException> invalidDtoException =
-                Optional.ofNullable((InvalidDtoException) result.getResolvedException());
-
-        invalidDtoException.ifPresent((e) -> assertThat(e, is(notNullValue())));
-        invalidDtoException.ifPresent((e) -> assertThat(e, is(instanceOf(InvalidDtoException.class))));
+        assertThat(result.getResolvedException(), is(instanceOf(InvalidDtoException.class)));
     }
 
     @Test
-    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @WithMockUser(username = USERNAME_ADMIN, roles = {ROLE_ADMIN})
     @Transactional
     @DataSet("database_init.yml")
     void createCategory_whenInternalSubcategoryIdNotEqualsZero_shouldReturnBadRequest()
             throws Exception {
+
         CategoryDto nonExistCategoryDtoWithInvalidId = createNonExistCategoryDtoWithInvalidSubcategoryId();
 
         mockMvc.perform(post("/category")
@@ -160,12 +156,13 @@ public class CategoryControllerIntegrationTest {
     }
 
     @Test
-    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @WithMockUser(username = USERNAME_ADMIN, roles = {ROLE_ADMIN})
     @Transactional
     @DataSet("database_init.yml")
     void createCategory_whenCategoryNameHasDuplicate_shouldReturnBadRequest()
             throws Exception {
-        CategoryDto nonExistCategoryDtoWithDuplicateName = createNonExistCategoryDtoWithDuplicateName();
+
+        CategoryDto nonExistCategoryDtoWithDuplicateName = createCategoryDtoWithDuplicateName();
 
         mockMvc.perform(post("/category")
                 .content(asJsonString(nonExistCategoryDtoWithDuplicateName))
@@ -176,17 +173,18 @@ public class CategoryControllerIntegrationTest {
     }
 
     @Test
-    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @WithMockUser(username = USERNAME_ADMIN, roles = {ROLE_ADMIN})
     @Transactional
     @DataSet("database_init.yml")
     @ExpectedDataSet(value = "category/update_category.yml")
     void updateCategory_shouldUpdateExistedCategory() throws Exception {
-        CategoryDto existCategoryDto = createExistCategoryDto();
-        existCategoryDto.setSubcategories(Arrays.asList(new SubcategoryDto(1L, "light shoes"),
-                new SubcategoryDto(0L, "winter shoes")));
-        existCategoryDto.setName("footwear");
+
+        CategoryDto updatedCategoryDto = getUpdatedCategoryDto(EXISTING_ENTITY_ID,
+                EXISTING_ENTITY_ID,
+                "footwear");
+
         mockMvc.perform(put("/category")
-                .content(asJsonString(existCategoryDto))
+                .content(asJsonString(updatedCategoryDto))
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
@@ -196,51 +194,40 @@ public class CategoryControllerIntegrationTest {
     }
 
     @Test
-    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @WithMockUser(username = USERNAME_ADMIN, roles = {ROLE_ADMIN})
     @Transactional
     @DataSet("database_init.yml")
     void updateCategory_whenCategoryIdDoesNotExist_shouldReturnBadRequestAndThrowIllegalIdentifierException()
             throws Exception {
-        long nonExistentCategoryId = 100000001L;
-        CategoryDto existCategoryDto = createExistCategoryDto();
-        existCategoryDto.setSubcategories(Arrays.asList(new SubcategoryDto(1L, "men shoes"),
-                new SubcategoryDto(0L, "women shoes")));
-        existCategoryDto.setId(nonExistentCategoryId);
 
-        String errorMessage = "The updated category and subcategories have " +
-                "to exist by id, new subcategories have to be 0 and also category name mustn't have duplicates " +
-                "except current name!";
+        CategoryDto updatedCategoryDto = getUpdatedCategoryDto(EXISTING_ENTITY_ID,
+                NONEXISTENT_ENTITY_ID,
+                NEW_CATEGORY_NAME);
 
         MvcResult result = mockMvc.perform(put("/category")
-                .content(asJsonString(existCategoryDto))
+                .content(asJsonString(updatedCategoryDto))
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isBadRequest())
-                .andExpect(content().string(containsString(errorMessage)))
                 .andReturn();
 
-        Optional<IllegalIdentifierException> illegalIdentifierException =
-                Optional.ofNullable((IllegalIdentifierException) result.getResolvedException());
-
-        illegalIdentifierException.ifPresent((e) -> assertThat(e, is(notNullValue())));
-        illegalIdentifierException.ifPresent((e) -> assertThat(e, is(instanceOf(IllegalIdentifierException.class))));
+        assertThat(result.getResolvedException(), is(instanceOf(IllegalIdentifierException.class)));
     }
 
     @Test
-    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @WithMockUser(username = USERNAME_ADMIN, roles = {ROLE_ADMIN})
     @Transactional
     @DataSet("database_init.yml")
     void updateCategory_whenSubcategoryIdDoesNotExistAndNotEqualsZero_shouldReturnBadRequest()
             throws Exception {
-        long nonExistentSubcategoryId = 111111L;
-        CategoryDto existCategoryDto = createExistCategoryDto();
-        existCategoryDto.setSubcategories(Arrays.asList(new SubcategoryDto(nonExistentSubcategoryId, "men shoes"),
-                new SubcategoryDto(0L, "women shoes")));
-        existCategoryDto.setName("new name");
+
+        CategoryDto updatedCategoryDto = getUpdatedCategoryDto(NONEXISTENT_ENTITY_ID,
+                EXISTING_ENTITY_ID,
+                NEW_CATEGORY_NAME);
 
         mockMvc.perform(put("/category")
-                .content(asJsonString(existCategoryDto))
+                .content(asJsonString(updatedCategoryDto))
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
@@ -248,7 +235,7 @@ public class CategoryControllerIntegrationTest {
     }
 
     @Test
-    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @WithMockUser(username = USERNAME_ADMIN, roles = {ROLE_ADMIN})
     @Transactional
     @DataSet("database_init.yml")
     void deleteCategoryById_shouldDeleteExistedCategory() throws Exception {
@@ -260,66 +247,31 @@ public class CategoryControllerIntegrationTest {
     }
 
     @Test
-    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @WithMockUser(username = USERNAME_ADMIN, roles = {ROLE_ADMIN})
     @Transactional
     @DataSet("database_init.yml")
     void deleteCategory_whenCategoryIdDoesNotExist_shouldReturnBadRequestAndThrowInvalidDtoException()
             throws Exception {
-        long nonExistentCategoryId = 100000021L;
-        String errorMessage = "The category can not be deleted by this id, " +
-                "because it has to exist by id and it's subcategories mustn't have products! Given category id: ";
 
-        MvcResult result = this.mockMvc.perform(delete("/category/{category_id}", nonExistentCategoryId)
+        MvcResult result = this.mockMvc.perform(delete("/category/{category_id}", NONEXISTENT_ENTITY_ID)
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isBadRequest())
-                .andExpect(content().string(containsString(errorMessage)))
                 .andReturn();
 
-        Optional<InvalidDtoException> invalidDtoException =
-                Optional.ofNullable((InvalidDtoException) result.getResolvedException());
-
-        invalidDtoException.ifPresent((e) -> assertThat(e, is(notNullValue())));
-        invalidDtoException.ifPresent((e) -> assertThat(e, is(instanceOf(InvalidDtoException.class))));
+        assertThat(result.getResolvedException(), is(instanceOf(InvalidDtoException.class)));
     }
 
     @Test
-    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @WithMockUser(username = USERNAME_ADMIN, roles = {ROLE_ADMIN})
     @Transactional
     @DataSet("database_init.yml")
     void deleteCategory_whenInternalSubcategoryHasProducts_shouldReturnBadRequest()
             throws Exception {
-        mockMvc.perform(delete("/category/{category_id}", 1L)
+
+        mockMvc.perform(delete("/category/{category_id}", EXISTING_ENTITY_ID)
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isBadRequest());
-    }
-
-    private CategoryDto createExistCategoryDto() {
-        SubcategoryDto lightShoes = new SubcategoryDto(1L, "light_shoes");
-        return new CategoryDto(1L, "shoes", Collections.singletonList(lightShoes));
-    }
-
-    private CategoryDto createNonExistValidCategoryDto() {
-        SubcategoryDto fairyTales = new SubcategoryDto(0L, "fairy tales");
-        SubcategoryDto educationalBooks = new SubcategoryDto(0L, "educational books");
-        return new CategoryDto(0L, "books", Arrays.asList(fairyTales, educationalBooks));
-    }
-
-    private CategoryDto createNonExistCategoryDtoWithInvalidId() {
-        SubcategoryDto validSubcategory = new SubcategoryDto(0L, "valid subcategory");
-        return new CategoryDto(1L, "invalid", Collections.singletonList(validSubcategory));
-    }
-
-    private CategoryDto createNonExistCategoryDtoWithInvalidSubcategoryId() {
-        SubcategoryDto validSubcategory = new SubcategoryDto(0L, "valid subcategory");
-        SubcategoryDto invalidSubcategory = new SubcategoryDto(1L, "invalid subcategory");
-        return new CategoryDto(0L, "invalid category", Arrays.asList(validSubcategory, invalidSubcategory));
-    }
-
-    private CategoryDto createNonExistCategoryDtoWithDuplicateName() {
-        SubcategoryDto validSubcategory = new SubcategoryDto(0L, "valid subcategory");
-        SubcategoryDto otherValidSubcategory = new SubcategoryDto(0L, "valid subcategory");
-        return new CategoryDto(0L, "shoes", Arrays.asList(validSubcategory, otherValidSubcategory));
     }
 }
