e2f824d0b48886e4a11bdd9aab989e618994d7bc
LebedevMichael <LebedevMichael1990@gmail.com>
2020-03-19 22:51:02 +0200

    CGE-64 small improvements #2

--- src/main/java/com/hillel/items_exchange/controller/ProfilePageController.java
+++ src/main/java/com/hillel/items_exchange/controller/ProfilePageController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/profile")
 public class ProfilePageController {
-    @GetMapping()
+    @GetMapping
     public String getProfile() {
         return "profile";
     }
