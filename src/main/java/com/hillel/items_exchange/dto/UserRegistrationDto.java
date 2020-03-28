3d733c67103042ca7c0cdb447163aa06375d41a9
Yevhenii Petrov <evgeniy.jyk@gmail.com>
2020-03-28 22:40:06 +0200

    Fixed registration validation

--- src/main/java/com/hillel/items_exchange/dto/UserRegistrationDto.java
+++ src/main/java/com/hillel/items_exchange/dto/UserRegistrationDto.java
@@ -4,13 +4,31 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserRegistrationDto {
 
+    @NotEmpty(message = "{empty.username}")
+    @Size(min = 2, max = 50, message = "{invalid.username.size}")
+    @Pattern(regexp = "(?=\\S+$).{2,50}", message = "{invalid.username}")
     private String username;
+
+    @NotEmpty(message = "{empty.email}")
+    @Size(max = 129, message = "{too.big.email}")
+    @Email(regexp = "^[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-zA-Z]{2,})$", message = "{invalid.password}")
     private String email;
+
+    @NotEmpty(message = "{empty.password}")
+    @Size(min = 8, max = 30, message = "{invalid.password}")
+    @Pattern(regexp = "(?=.*?[0-9])(?=.*?[a-z])(?=.*?[A-Z])(?=\\S+$).+.{8,30}", message = "{invalid.password}")
     private String password;
+
+    @NotEmpty(message = "{empty.confirm.password}")
     private String confirmPassword;
 }
