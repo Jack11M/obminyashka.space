7ea65770e6fc3ebe2030c58b828d5dcc8ca09152
Andrey Teslenko <andteslenko@gmail.com>
2020-04-16 21:47:22 +0300

    CGE-55: Changed project's structure and removed excess controllers

---
+++ src/main/resources/react/src/components/pages/hooks/doFetch.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default (controller, whatPage) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [response, setResponse] = useState(null);
+  const [errorsResponse, setError] = useState(null);
+  const [data, setData] = useState({});
+
+  let baseUrl = `http://54.37.125.180:8080/${controller}/${whatPage}`;
+  // let baseUrl = `http://localhost:8080/${controller}/${whatPage}`;
+  const headers = { "Content-Type": "application/json" };
+
+  useEffect(() => {
+    if (!isLoading) {
+      return;
+    }
+
+    axios({
+      method: "post",
+      url: baseUrl,
+      data,
+      headers
+    })
+      .then(res => {
+        console.log(res.data);
+        setIsLoading(false);
+        setResponse(res.data);
+      })
+      .catch(errors => {
+        console.log("ErrorFromBase", errors.response.data);
+        setIsLoading(false);
+        setError(errors.response.data);
+      });
+  });
+
+  const doFetch = data => {
+    setData(data);
+    setIsLoading(true);
+  };
+  return [{ isLoading, response, errorsResponse }, doFetch];
+};
