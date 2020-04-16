022d2797051494047628ee4faa4332dd7864b0e0
Roman Kyrychenko <roman.kyrychenko@cengage.com>
2020-04-16 23:37:46 +0300

    CGE-55: Removed old React folder and few pages controllers that aren't needed anymore

--- src/main/react/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-
-import "./index.scss";
-import "./fonts/fontIcon.scss";
-
-ReactDOM.render(<App />, document.getElementById("root"));
