3d7a75badfcbb2ad781838ad0855438952063654
Lubchik <lubchik.14@gmail.com>
2020-04-23 13:37:17 +0300

    CGE-51 Simplified requests

--- src/main/java/com/hillel/items_exchange/dao/ImageRepository.java
+++ src/main/java/com/hillel/items_exchange/dao/ImageRepository.java
@@ -2,7 +2,6 @@ package com.hillel.items_exchange.dao;
 
 import com.hillel.items_exchange.model.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,9 +9,6 @@ import java.util.List;
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
 
-    @Query(value = "SELECT i.resource_url FROM image i, advertisement a WHERE a.id = ?1 AND a.product_id = ?2 AND i.product_id = a.product_id", nativeQuery = true)
-    List<String> findImageUrlsByAdvertisementIdAndProductId(Long advId, Long productId);
+    List<Image> findByProductId(Long id);
 
-    @Query(value = "SELECT i.* FROM image i, advertisement a WHERE a.id = ?1 AND a.product_id = ?2 AND i.product_id = a.product_id", nativeQuery = true)
-    List<Image> findByAdvertisementIdAndProductId(Long advId, Long productId);
 }
