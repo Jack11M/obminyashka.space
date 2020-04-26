a9226ba912d0772884d99b510786b8cd28305c85
LebedevMichael <LebedevMichael1990@gmail.com>
2020-04-26 18:11:34 +0300

    CGE-70 Added to the field of the Principle method and organized a check by id. Fixed test class

--- src/main/java/com/hillel/items_exchange/controller/UserController.java
+++ src/main/java/com/hillel/items_exchange/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.hillel.items_exchange.controller;
 
 import com.hillel.items_exchange.dto.UserDto;
+import com.hillel.items_exchange.model.User;
 import com.hillel.items_exchange.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/user")
@@ -19,7 +22,13 @@ public class UserController {
 
     @RequestMapping(value = "/info/{id}", method = RequestMethod.GET)
     public @ResponseBody
-    ResponseEntity<UserDto> getUser(@PathVariable("id") Long id) {
+    ResponseEntity<UserDto> getUserDto(@PathVariable("id") Long id, Principal principal) {
+
+        User user = userService.findByUsernameOrEmail(principal.getName()).get();
+
+        if (user != null && user.getId() != id){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
 
         return userService.getUserDtoById(id)
                 .map(userDto -> new ResponseEntity<>(userDto, HttpStatus.OK))
--- src/test/java/com/hillel/items_exchange/controller/UserControllerTest.java
+++ src/test/java/com/hillel/items_exchange/controller/UserControllerTest.java
@@ -7,10 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.transaction.Transactional;
 
+import static com.hillel.items_exchange.util.JsonConverter.asJsonString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -19,16 +22,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @DBRider
 @AutoConfigureMockMvc
+@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:index-reset.sql")
 class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
+    @WithMockUser(username = "admin")
     @Transactional
     @DataSet("database_init.yml")
     void getUserDto_shouldReturnUserDtoIfExists() throws Exception {
         mockMvc.perform(get("/user/info/{id}", 1L)
+                .content(asJsonString("admin"))
+                .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(jsonPath("$.id").value(1))
