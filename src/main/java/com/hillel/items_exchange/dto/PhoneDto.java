6531ab5e82afa4feb3bac61b5996b3f02ae7097c
Lubchik <lubchik.14@gmail.com>
2020-06-13 22:09:59 +0300

    CGE-88 Added id fields

--- src/main/java/com/hillel/items_exchange/dto/PhoneDto.java
+++ src/main/java/com/hillel/items_exchange/dto/PhoneDto.java
@@ -1,16 +1,21 @@
 package com.hillel.items_exchange.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.PositiveOrZero;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
-@EqualsAndHashCode
 public class PhoneDto {
+    @PositiveOrZero(message = "{invalid.id}")
+    private long id;
     @NotNull(message = "{invalid.not-null}")
     private Boolean show;
     @Pattern(regexp = "^\\s*(?<country>\\+?\\d{1,3})[-. (]*(?<area>\\d{3})[-. )]*(?<number>\\d{3}[-. ]*\\d{2}[-. ]*\\d{2})\\s*$",
