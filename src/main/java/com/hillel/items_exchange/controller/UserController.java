d344c607a82cf0bd7c569061f4eb405da63c4d07
Lubchik <lubchik.14@gmail.com>
2020-06-13 22:09:08 +0300

    CGE-88 Changed retrieving a registered user

--- src/main/java/com/hillel/items_exchange/controller/UserController.java
+++ src/main/java/com/hillel/items_exchange/controller/UserController.java
@@ -1,25 +1,23 @@
 package com.hillel.items_exchange.controller;
 
-import java.security.Principal;
-import java.util.Locale;
-
-import javax.validation.constraints.PositiveOrZero;
-import javax.validation.Valid;
-
+import com.hillel.items_exchange.dto.UserDto;
+import com.hillel.items_exchange.security.jwt.JwtUser;
+import com.hillel.items_exchange.service.UserService;
+import com.hillel.items_exchange.util.MessageSourceUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-import com.hillel.items_exchange.dto.UserDto;
-import com.hillel.items_exchange.service.UserService;
-import com.hillel.items_exchange.util.MessageSourceUtil;
+import javax.validation.Valid;
+import javax.validation.constraints.PositiveOrZero;
+import java.security.Principal;
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/user")
@@ -48,24 +46,12 @@ public class UserController {
     @PutMapping("/info/{id}")
     public ResponseEntity<UserDto> updateUserInfo(@Valid @RequestBody UserDto userDto,
                                                   @PositiveOrZero @PathVariable("id") long id,
-                                                  Principal principal) {
-        //TODO get user Id instead of nameOrEmail
-        UserDto user = this.getUserDto(principal.getName());
-        validateInfoOwner(user.getId(), id);
-        return new ResponseEntity<>(userService.update(userDto), HttpStatus.OK);
-    }
-
-    private UserDto getUserDto(String nameOrEmail) {
-        return userService.getByUsernameOrEmail(nameOrEmail)
-                .orElseThrow(() -> new UsernameNotFoundException(
-                        messageSourceUtil.getExceptionMessageSourceWithAdditionalInfo(
-                                "user.not-found", nameOrEmail)
-                ));
-    }
-
-    private void validateInfoOwner(long loginUserId, long userId) {
-        if (loginUserId != userId)
-            throw new SecurityException(
-                    messageSourceUtil.getExceptionMessageSourceWithId(userId, "user.not.allowed"));
+                                                  @AuthenticationPrincipal JwtUser user) {
+        Long userId = user.getId();
+        if (userId != id || userId != userDto.getId()) {
+            throw new AccessDeniedException(
+                    messageSourceUtil.getExceptionMessageSource("exception.permission-denied.user-profile"));
+        }
+        return new ResponseEntity<>(userService.update(userDto), HttpStatus.ACCEPTED);
     }
 }
