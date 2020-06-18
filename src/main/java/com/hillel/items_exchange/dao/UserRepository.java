f0637afb0ca1e1ff12c8bc6df0ea8671ee9b2689
Lubchik <lubchik.14@gmail.com>
2020-06-18 16:49:24 +0300

    CGE-88 Deleted useless implementation

--- src/main/java/com/hillel/items_exchange/dao/UserRepository.java
+++ src/main/java/com/hillel/items_exchange/dao/UserRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long>, JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findById(Long id);
 
