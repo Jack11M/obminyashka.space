ecb4f37170996e4f5c60bad84dd11e4987ad2463
Lubchik <lubchik.14@gmail.com>
2020-06-23 17:22:58 +0300

    CGE-88 Changed regexp pattern cover only Ukrainian mobile phone numbers

--- src/main/java/com/hillel/items_exchange/util/PatternHandler.java
+++ src/main/java/com/hillel/items_exchange/util/PatternHandler.java
@@ -4,6 +4,6 @@ public class PatternHandler {
     public static final String EMAIL = "^[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-zA-Z]{2,})$";
     public static final String PASSWORD_MIN_7_MAX_30 = "(?=.*?[0-9])(?=.*?[a-z])(?=.*?[A-Z])(?=\\S+$).+.{7,30}";
     public static final String PHONE_NUMBER =
-            "^\\s*(?<country>\\+?\\d{1,3})[-. (]*(?<area>\\d{3})[-. )]*(?<number>\\d{3}[-. ]*\\d{2}[-. ]*\\d{2})\\s*$";
+            "^\\s*(?<country>\\+?\\d{2})[-. (]*(?<area>\\d{3})[-. )]*(?<number>\\d{3}[-. ]*\\d{2}[-. ]*\\d{2})\\s*$";
     public static final String USERNAME_MIN_2_MAX_50 = "(?=\\S+$).{2,50}";
 }
