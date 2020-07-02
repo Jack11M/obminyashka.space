12b18a86e3a28043ffa8ffe31e171e80135e7417
Roman Kyrychenko <roman.kyrychenko@cengage.com>
2020-07-02 22:00:39 +0300

    CGE-90: Removed excess method

--- src/main/java/com/hillel/items_exchange/dao/LocationRepository.java
+++ src/main/java/com/hillel/items_exchange/dao/LocationRepository.java
@@ -4,10 +4,7 @@ import com.hillel.items_exchange.model.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
-
-    Optional<Location> findByCityAndDistrict(String city, String district);
+    
 }
