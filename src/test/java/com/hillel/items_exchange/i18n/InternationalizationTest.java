ddf49f16547b881187c8f68c9f99e628191dce1e
Lubchik <lubchik.14@gmail.com>
2020-08-10 11:38:33 +0300

    CGE-95 Changed russian messages from messages_ru.properties, removed redundant

--- src/test/java/com/hillel/items_exchange/i18n/InternationalizationTest.java
+++ src/test/java/com/hillel/items_exchange/i18n/InternationalizationTest.java
@@ -1,6 +1,5 @@
 package com.hillel.items_exchange.i18n;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,8 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -17,20 +14,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class InternationalizationTest {
+class InternationalizationTest {
 
-    @Autowired
-    private WebApplicationContext context;
-
+@Autowired
     private MockMvc mockMvc;
 
-    @BeforeEach
-    void setup() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-    }
-
     @Test
-    public void testDefaultLocalizationWithNotExistedAccept_LanguageHeader() throws Exception {
+    void testDefaultLocalizationWithNotExistedAccept_LanguageHeader() throws Exception {
         MvcResult mvcResult = mockMvc.perform(get("/image/{product_id}", -100L)
                 .header("Accept-Language", "notExisted")
                 .accept(MediaType.APPLICATION_JSON))
@@ -42,7 +32,7 @@ public class InternationalizationTest {
     }
 
     @Test
-    public void testDefaultLocalizationWithoutAccept_LanguageHeader() throws Exception {
+    void testDefaultLocalizationWithoutAccept_LanguageHeader() throws Exception {
         MvcResult mvcResult = mockMvc.perform(get("/image/{product_id}", -100L)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest())
@@ -53,7 +43,7 @@ public class InternationalizationTest {
     }
 
     @Test
-    public void testRussianLocalizationWithAccept_LanguageHeaderIsRu() throws Exception {
+    void testRussianLocalizationWithAccept_LanguageHeaderIsRu() throws Exception {
         MvcResult mvcResult = mockMvc.perform(get("/image/{product_id}", -100L)
                 .header("Accept-Language", "ru")
                 .accept(MediaType.APPLICATION_JSON))
@@ -65,7 +55,7 @@ public class InternationalizationTest {
     }
 
     @Test
-    public void testRussianLocalizationWithAccept_LanguageHeaderIsRu_Ru() throws Exception {
+    void testRussianLocalizationWithAccept_LanguageHeaderIsRu_Ru() throws Exception {
         MvcResult mvcResult = mockMvc.perform(get("/image/{product_id}", -100L)
                 .header("Accept-Language", "ru-Ru")
                 .accept(MediaType.APPLICATION_JSON))
