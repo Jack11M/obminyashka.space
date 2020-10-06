89620a5135c26786b0b34d06a04519a854248089
Gaiver <gaiverkostyan1987@gmail.com>
2020-10-06 12:35:47 +0300

    CGE-117 Simplified annotations @ApiModelProperty and @NotEmpty to @NotNull

--- src/main/java/com/hillel/items_exchange/dto/ProductDto.java
+++ src/main/java/com/hillel/items_exchange/dto/ProductDto.java
@@ -1,10 +1,7 @@
 package com.hillel.items_exchange.dto;
-
-import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
 import javax.validation.constraints.Size;
@@ -18,20 +15,16 @@ import java.util.List;
 public class ProductDto {
     @PositiveOrZero(message = "{invalid.id}")
     private long id;
-    @ApiModelProperty(required = true)
-    @NotEmpty(message = "{invalid.not-empty}")
+    @NotNull(message = "{invalid.not-null}")
     @Size(min = 1, max = 50, message = "{invalid.size}")
     private String age;
-    @ApiModelProperty(required = true)
-    @NotEmpty(message = "{invalid.not-empty}")
+    @NotNull(message = "{invalid.not-null}")
     @Size(min = 1, max = 50, message = "{invalid.size}")
     private String gender;
-    @ApiModelProperty(required = true)
-    @NotEmpty(message = "{invalid.not-empty}")
+    @NotNull(message = "{invalid.not-null}")
     @Size(min = 1, max = 50, message = "{invalid.size}")
     private String season;
-    @ApiModelProperty(required = true)
-    @NotEmpty(message = "{invalid.not-empty}")
+    @NotNull(message = "{invalid.not-null}")
     @Size(min = 1, max = 50, message = "{invalid.size}")
     private String size;
     @NotNull(message = "{invalid.not-null}")
