1c224c724039fdd194f25b1f67bb1e3c064871db
Svyatoslav <slav145@ukr.net>
2020-10-06 19:35:28 +0300

    CGE-135 Add tests for WORD_EMPTY_OR_MIN_2_MAX_50 regexp, USERNAME regexp; change PatternHandler

--- src/main/java/com/hillel/items_exchange/util/PatternHandler.java
+++ src/main/java/com/hillel/items_exchange/util/PatternHandler.java
@@ -10,6 +10,7 @@ public class PatternHandler {
     public static final String PASSWORD = "(?=.*?[0-9])(?=.*?[a-z])(?=.*?[A-Z])(?=\\S+$)[\\w\\p{Punct}]+";
     public static final String PHONE_NUMBER =
             "^\\s*(?<country>\\+?\\d{2})[-. (]*(?<area>\\d{3})[-. )]*(?<number>\\d{3}[-. ]*\\d{2}[-. ]*\\d{2})\\s*$";
-    public static final String USERNAME = "(?=\\S+$)[\\wА-Яа-я\\p{Punct}]+";
-    public static final String WORD_EMPTY_OR_MIN_2_MAX_50 = "^$|^[\\w-']{2,50}+$";
+    public static final String USERNAME = "(?=\\S+$)[\\wА-Яа-яЁёҐЄІЇієїґ\\p{Punct}]+";
+    //    public static final String WORD_EMPTY_OR_MIN_2_MAX_50 = "^$|^[\\w-']{2,50}+$";
+    public static final String WORD_EMPTY_OR_MIN_2_MAX_50 = "^$|^[\\wА-Яа-я-'`ЁёҐЄІЇієїґ]{2,50}+$";
 }
