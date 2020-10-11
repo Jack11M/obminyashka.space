f2c2f5f002181d4f3fdcc91a7da1c2a9af7b588b
Dariy98 <ostapenko.d98@gmail.com>
2020-10-11 16:43:56 +0300

    CGE-33: Ui of my settings

--- src/main/resources/react/src/pages/UserInfo/tab_pages/my_settings/mySettings.js
+++ src/main/resources/react/src/pages/UserInfo/tab_pages/my_settings/mySettings.js
@@ -1,7 +1,49 @@
 import React from "react";
 
+import TitleBigBlue from "../../../../components/title_Big_Blue/title_Big_Blue";
+import Button from '../../../../components/button/Button';
+import "./mySettings.scss";
+
 const MySettings = () => {
-  return <div>мои настройки</div>;
+    return (
+        <form>
+            <TitleBigBlue whatClass={"myProfile-title"} text={"Сменить пароль"}/>
+            <div className="info-one">
+                <div className="row">
+                    <label htmlFor="name">Текущий пароль:</label>
+                    <input type="password" name="name" id="name"/>
+                </div>
+                <div className="row">
+                    <label htmlFor="lastName">Новый пароль:</label>
+                    <input type="password" name="lastName" id="lastName"/>
+                </div>
+                <div className="row">
+                    <label htmlFor="city">Повторите пароль:</label>
+                    <input type="password" name="city" id="city"/>
+                </div>
+            </div>
+            <Button text={"Сохранить"} whatClass={"btn-profile"}/>
+            <TitleBigBlue whatClass={"myProfile-title"} text={"Сменить e-mail"}/>
+            <div className="info-one">
+                <div className="row">
+                    <label htmlFor="name">Старый E-Mail:</label>
+                    <input type="email" name="name" id="name"/>
+                </div>
+                <div className="row">
+                    <label htmlFor="lastName">Новый E-Mail:</label>
+                    <input type="email" name="lastName" id="lastName"/>
+                </div>
+            </div>
+            <Button text={"Отправить код подтверждения"} whatClass={"btn-profile e-mail-button"}/>
+            <TitleBigBlue whatClass={"myProfile-title"} text={"Удалить аккаунт"}/>
+            <p className="delete-text">
+                Внимание! После удаления учетной записи, ваш профиль будет полностью удален из каталога сайта
+                и многие функции перестанут быть доступными. Вы можете поменять контактные данные и номера телефонов
+                в разделе меню <a href="#">Профиль</a>
+            </p>
+            <Button text={"Удалить аккаунт"} whatClass={"btn-profile"}/>
+        </form>
+    )
 };
 
 export default MySettings;
