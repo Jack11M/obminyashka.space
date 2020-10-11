3534239184808fbf4f3db9dc761187f303dc650c
Dariy98 <ostapenko.d98@gmail.com>
2020-10-11 20:31:18 +0300

    CGE-33 replace div on InputProfile component

--- src/main/resources/react/src/pages/UserInfo/tab_pages/my_settings/mySettings.js
+++ src/main/resources/react/src/pages/UserInfo/tab_pages/my_settings/mySettings.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import TitleBigBlue from "../../../../components/title_Big_Blue/title_Big_Blue";
 import Button from '../../../../components/button/Button';
+import InputProfile from '../../components/inputProfile/inputProfile';
 import "./mySettings.scss";
 
 const MySettings = () => {
@@ -9,30 +10,15 @@ const MySettings = () => {
         <form>
             <TitleBigBlue whatClass={"myProfile-title"} text={"Сменить пароль"}/>
             <div className="info-one">
-                <div className="row">
-                    <label htmlFor="name">Текущий пароль:</label>
-                    <input type="password" name="name" id="name"/>
-                </div>
-                <div className="row">
-                    <label htmlFor="lastName">Новый пароль:</label>
-                    <input type="password" name="lastName" id="lastName"/>
-                </div>
-                <div className="row">
-                    <label htmlFor="city">Повторите пароль:</label>
-                    <input type="password" name="city" id="city"/>
-                </div>
+                <InputProfile data={{name: 'currentPassword', label: 'Текущий пароль:', type: 'password'}}/>
+                <InputProfile data={{name: 'newPassword', label: 'Новый пароль:', type: 'password'}}/>
+                <InputProfile data={{name: 'newPasswordRepeat', label: 'Повторите пароль:', type: 'password'}}/>
             </div>
             <Button text={"Сохранить"} whatClass={"btn-profile"}/>
             <TitleBigBlue whatClass={"myProfile-title"} text={"Сменить e-mail"}/>
             <div className="info-one">
-                <div className="row">
-                    <label htmlFor="name">Старый E-Mail:</label>
-                    <input type="email" name="name" id="name"/>
-                </div>
-                <div className="row">
-                    <label htmlFor="lastName">Новый E-Mail:</label>
-                    <input type="email" name="lastName" id="lastName"/>
-                </div>
+                <InputProfile data={{name: 'mail', label: 'Старый E-Mail:', type: 'email'}}/>
+                <InputProfile data={{name: 'newMail', label: 'Новый E-Mail:', type: 'email'}}/>
             </div>
             <Button text={"Отправить код подтверждения"} whatClass={"btn-profile e-mail-button"}/>
             <TitleBigBlue whatClass={"myProfile-title"} text={"Удалить аккаунт"}/>
