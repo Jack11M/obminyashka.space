d0ce651870f951f106ef6796a56922e669d0b41d
Dariy98 <ostapenko.d98@gmail.com>
2020-11-01 18:54:00 +0300

    CGE-32: UI of my favorites

--- src/main/resources/react/src/pages/UserInfo/tab_pages/my_favorites/myFavorites.js
+++ src/main/resources/react/src/pages/UserInfo/tab_pages/my_favorites/myFavorites.js
@@ -1,7 +1,39 @@
 import React from "react";
 
+import "./myFavorites.scss";
+import TitleBigBlue from "../../../../components/title_Big_Blue/title_Big_Blue";
+import CardWithStar from "../../components/cards/card-with-star/card-with-star";
+
 const MyFavorites = () => {
-  return <div>Избранное</div>;
+    const arr = [{
+        elem: {
+            title: "Название",
+            city: "Киев",
+            subject: "https://static.toiimg.com/photo/72975551.cms"
+        }
+    }];
+
+    return (
+        <section id="content3" className="tabs__content">
+            <div className="content">
+                <TitleBigBlue whatClass={"myProfile-title"} text={"Избранные объявления"}/>
+                <div className="cards_replies">
+                    {arr.map(elem => {
+                        return (
+                            <>
+                                <CardWithStar title={elem.title} city={elem.city} subject={elem.subject}/>
+                                <CardWithStar/>
+                                <CardWithStar/>
+                                <CardWithStar/>
+                                <CardWithStar/>
+                                <CardWithStar/>
+                            </>
+                        )
+                    })}
+                </div>
+            </div>
+        </section>
+    )
 };
 
 export default MyFavorites;
