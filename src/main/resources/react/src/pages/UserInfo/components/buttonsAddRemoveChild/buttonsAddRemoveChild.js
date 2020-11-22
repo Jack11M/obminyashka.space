0d6a4212d3e533ea76e9fb80736e440443287929
Andrey Teslenko <andteslenko@gmail.com>
2020-11-22 19:20:28 +0200

    CGE-30: Added input checkbox

--- src/main/resources/react/src/pages/UserInfo/components/buttonsAddRemoveChild/buttonsAddRemoveChild.js
+++ src/main/resources/react/src/pages/UserInfo/components/buttonsAddRemoveChild/buttonsAddRemoveChild.js
@@ -1,12 +1,63 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const ButtonsAddRemoveChild = (props) => {
-	const {whatIsClass, text, classSpan, click} = props;
+const AddRemoveItem = styled.div`
+	display: inline-flex;
+  align-items: center;
+  span:first-child {
+    font-size: 14px;
+    line-height: 16px;
+    margin-right: 21px;
+  }
+`;
+const Span = styled.span`
+	position: relative;
+  display: inline-flex;
+  width: 34px;
+  height: 34px;
+  color: #fff;
+  border-radius: 50%;
+  background-color: ${ props => props.addRemove ? 'hsl(134, 45%, 48%)' : 'hsl(0, 0%, 47%)' };
+  transition: 0.2s;
+  &:hover {
+    cursor: pointer;
+    background-color: ${ props => props.addRemove ? 'hsl(134, 45%, 43%)' : 'hsl(0, 0%, 42%)' };
+  }
+  
+  :before {
+	  position: absolute;
+	  content: "";
+	  top: 16px;
+    left: 12px;
+    width: 10px;
+    height: 2px;
+	  background-color: #fff;
+	  transform: ${ props => props.addRemove ? 'rotate(0deg)' : 'rotate(45deg)' };
+  }
+  :after {
+	  position: absolute;
+	  content: "";
+	  top: 12px;
+    left: 16px;
+    width: 2px;
+    height: 10px;
+	  background-color: #fff;
+	  transform: ${ props => props.addRemove ? 'rotate(0deg)' : 'rotate(45deg)' };
+  }
+`;
+
+const ButtonsAddRemoveChild = ( props ) => {
+	const { text, addRemove, click } = props;
 	return (
-		<div className={whatIsClass}>
-			<span>{text}</span> <span className={classSpan} onClick={click}/>
-		</div>
+		<AddRemoveItem>
+			<span>{ text }</span>
+			<Span
+
+				addRemove={ addRemove === 'add' }
+				onClick={ click }
+			/>
+		</AddRemoveItem>
 	);
-}
+};
 
 export default ButtonsAddRemoveChild;
