043e007c5743ee9aa8082732be63205b7a17a30f
Kozmin Oleksandr <kozminalexandr462@gmail.com>
2020-11-28 20:57:26 +0200

    CGE-118: Remove unused import

--- src/main/java/com/hillel/items_exchange/util/MessageSourceUtil.java
+++ src/main/java/com/hillel/items_exchange/util/MessageSourceUtil.java
@@ -7,9 +7,6 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 
-import java.text.MessageFormat;
-
-
 @Component
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class MessageSourceUtil {
