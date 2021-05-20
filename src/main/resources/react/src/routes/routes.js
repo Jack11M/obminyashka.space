7659ddcc510d0abfc8826396fc91b995239a2aad
Andrey Teslenko <andteslenko@gmail.com>
2021-05-20 21:55:07 +0300

    CGE-35: resolved merge conflict

--- src/main/resources/react/src/routes/routes.js
+++ src/main/resources/react/src/routes/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route , Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import HomePage from '../pages/homepage/HomePage';
@@ -7,26 +7,32 @@ import UserInfo from '../pages/UserInfo/UserInfo';
 import Auth from '../pages/FormRegister/Auth';
 import Protected from './protectedRoute';
 import FourOhFourPage from '../pages/ErrorPages/FourOhFourPage';
-import { route } from './routeConstants';
 import ProductPage from '../pages/ProductPage/ProductPage';
+import AddGoods from '../pages/AddGoods/AddGoods';
+import { route } from './routeConstants';
 
 const Routes = () => {
 	const { isAuthenticated } = useSelector( state => state.auth );
 
 	return <Switch>
-		<Route path = { route.home } component = { HomePage } exact/>
+		<Route path={ route.home } component={ HomePage } exact/>
 		<Protected
-			path = { route.login }
-			component = { Auth }
-			permission = { !isAuthenticated }
-			redirect = { route.home }/>
+			path={ route.login }
+			component={ Auth }
+			permission={ !isAuthenticated }
+			redirect={ route.home }/>
 		<Protected
-			path = { route.userInfo }
-			component = { UserInfo }
-			permission = { isAuthenticated }
-			redirect = { route.login }/>
-		<Route path = { route.productPage } component = { ProductPage }/>
-		<Route path = { route.noMatch } component = { FourOhFourPage }/>
+			path={ route.userInfo }
+			component={ UserInfo }
+			permission={ isAuthenticated }
+			redirect={ route.login }/>
+		<Protected
+			path={ route.addAdv }
+			component={ AddGoods }
+			permission={ isAuthenticated }
+			redirect={ route.login }/>
+		<Route path={ route.productPage } component={ ProductPage }/>
+		<Route path={ route.noMatch } component={ FourOhFourPage }/>
 	</Switch>;
 };
 
