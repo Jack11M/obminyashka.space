aa31822e885a74050d73ff4bb614376f6a84d8b2
basist92 <basist92@gmail.com>
2021-06-01 19:03:16 +0300

    OBMIN-15 create UserUpdateDto

---
+++ src/main/java/space/obminyashka/items_exchange/dto/UserUpdateDto.java
@@ -0,0 +1,29 @@
+package space.obminyashka.items_exchange.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+import space.obminyashka.items_exchange.util.PatternHandler;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserUpdateDto {
+
+    @ApiModelProperty(value = "Empty or 2-50 symbols", example = "Mariana, Мар'яна, Марьяна")
+    @NotNull(message = "{invalid.not-null}")
+    @Pattern(regexp = PatternHandler.WORD_EMPTY_OR_MIN_2_MAX_50, message = "{invalid.first-or-last.name}")
+    private String firstName;
+    @ApiModelProperty(value = "Empty or 2-50 symbols",
+            example = "Kvitka-Osnovianenko, Квітка-Основ'яненко, Квитка-Основьяненко")
+    @NotNull(message = "{invalid.not-null}")
+    @Pattern(regexp = PatternHandler.WORD_EMPTY_OR_MIN_2_MAX_50, message = "{invalid.first-or-last.name}")
+    private String lastName;
+    @NotNull(message = "{invalid.not-null}")
+    private Set<@Valid PhoneDto> phones;
+}
