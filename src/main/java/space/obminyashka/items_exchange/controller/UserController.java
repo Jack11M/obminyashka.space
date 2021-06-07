a72031e0ca780fc74c156999ef8cdfe8235c4426
Roman Kyrychenko <37712692+rpkyrych@users.noreply.github.com>
2021-06-07 12:25:34 +0300

    Obmin 15 create user update dto (#216)
    * OBMIN-15 add max.phones amount to the properties
    
    * OBMIN-15 add messages to properties
    
    * OBMIN-15 add max.phones amount to the test  properties
    
    * OBMIN-15 create UserUpdateDto
    
    * OBMIN-15  fixed update method in UserService
    
    * OBMIN-15 fixed updateUserInfo method in UserController
    
    * OBMIN-15 fixed tests
    
    * OBMIN-15 updated end2end UserFlowTest
    
    * OBMIN-15 updated logic for validation amount of phones
    
    * OBMIN-15 extract duplicate logic into methods in UserControllerTest
    
    * OBMIN-15 update test WhenUserHasStatusDeleted
    
    * OBMIN-15 clean code
    
    * OBMIN-15 clean code
    
    * OBMIN-15 change logic of the update method in UserService. refactor UserMapper.class and delete it
    
    * OBMIN-15 remove unnecessary quotes in messages.properties
    
    * OBMIN-15_update_userController
    
    * OBMIN-15 remove redundant logic in the UserService
    
    * [OBMIN-15] Added missed import
    
    Co-authored-by: basist92 <basist92@gmail.com>
    Co-authored-by: Basist92 <77827635+Basist92@users.noreply.github.com>

--- src/main/java/space/obminyashka/items_exchange/controller/UserController.java
+++ src/main/java/space/obminyashka/items_exchange/controller/UserController.java
@@ -22,6 +22,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import springfox.documentation.annotations.ApiIgnore;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
