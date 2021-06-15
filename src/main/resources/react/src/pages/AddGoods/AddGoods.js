19a1e72c2eca4e713770797125749309f32a9b39
Andrey Teslenko <andteslenko@gmail.com>
2021-06-15 09:56:56 +0300

    OBMIN-22: resolved conflicts at AddGoods

--- src/main/resources/react/src/pages/AddGoods/AddGoods.js
+++ src/main/resources/react/src/pages/AddGoods/AddGoods.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import { useDispatch, useSelector } from 'react-redux';
 import CheckBox from "../../components/common/checkbox/index";
-import { toggleCheckBox } from '../../redux/auth/action';
 import { books, clothes, furniture, kidsUpToYear, other, shoes, toys, transportForChildren } from '../../img/all_images_export/navItems';
 import ButtonAdv from "../../components/common/buttonAdv/ButtonAdv";
 import Button from "../../components/common/button/Button";
@@ -13,10 +12,8 @@ import "./AddGoods.scss";
 
 const AddGoods = () => {
 
-	const { lang, logCheckbox } = useSelector(state => state.auth);
-	const changeCheckBox = () => {
-		dispatch(toggleCheckBox({ logCheckbox: !logCheckbox }));
-	};
+	const { lang } = useSelector(state => state.auth);
+	const changeCheckBox = () => {};
 	const dispatch = useDispatch();
 	return (
 		<main className="add">
@@ -40,7 +37,7 @@ const AddGoods = () => {
 							</div>
 							<div className="sections_item">
 								<h5 className="sections_item-description">* Заголовок обьявления</h5>
-								<input type="text"></input>
+								<input type="text"/>
 							</div>
 						</div>
 					</div>
@@ -49,17 +46,17 @@ const AddGoods = () => {
 						<p className="change-description">* На что хотите обменяться?</p>
 						<p className="change-description">(введите фразу, а потом нажмите Enter)</p>
 						<div className="change_wrapper">
-							<div className="change_item">Ботинки зимние <span></span></div>
-							<div className="change_item">Кофта на девочку 7 лет<span></span></div>
+							<div className="change_item">Ботинки зимние <span/></div>
+							<div className="change_item">Кофта на девочку 7 лет<span/></div>
 							<div className="change_input-wrapper" >
-								<input className="change_input" type="text" placeholder="Введите на что хотите обменяться"></input>
+								<input className="change_input" type="text" placeholder="Введите на что хотите обменяться"/>
 							</div>
 						</div>
 						<CheckBox
 									text={'Рассмотрю Ваше предложение'}
 									margin={'0 0 15px 0'}
 									fs={'14px'}
-									checked={logCheckbox}
+									checked={false}
 									click={changeCheckBox}
 								/>
 					</div>
@@ -72,42 +69,42 @@ const AddGoods = () => {
 									text={'0'}
 									margin={'0 0 15px 0'}
 									fs={'14px'}
-									checked={logCheckbox}
+									checked={false}
 									click={changeCheckBox}
 								/>
 								<CheckBox
 									text={'1-2'}
 									margin={'0 0 15px 0'}
 									fs={'14px'}
-									checked={logCheckbox}
+									checked={false}
 									click={changeCheckBox}
 								/>
 								<CheckBox
 									text={'2-4'}
 									margin={'0 0 15px 0'}
 									fs={'14px'}
-									checked={logCheckbox}
+									checked={false}
 									click={changeCheckBox}
 								/>
 								<CheckBox
 									text={'5-7'}
 									margin={'0 0 15px 0'}
 									fs={'14px'}
-									checked={logCheckbox}
+									checked={false}
 									click={changeCheckBox}
 								/>
 								<CheckBox
 									text={'8-11'}
 									margin={'0 0 15px 0'}
 									fs={'14px'}
-									checked={logCheckbox}
+									checked={false}
 									click={changeCheckBox}
 								/>
 								<CheckBox
 									text={'11-14'}
 									margin={'0 0 15px 0'}
 									fs={'14px'}
-									checked={logCheckbox}
+									checked={false}
 									click={changeCheckBox}
 								/>
 							</div>
@@ -117,21 +114,21 @@ const AddGoods = () => {
 									text={'Женский'}
 									margin={'0 0 15px 0'}
 									fs={'14px'}
-									checked={logCheckbox}
+									checked={false}
 									click={changeCheckBox}
 								/>
 								<CheckBox
 									text={'Мужской'}
 									margin={'0 0 15px 0'}
 									fs={'14px'}
-									checked={logCheckbox}
+									checked={false}
 									click={changeCheckBox}
 								/>
 								<CheckBox
 									text={'Мальчик/Девочка'}
 									margin={'0 0 15px 0'}
 									fs={'14px'}
-									checked={logCheckbox}
+									checked={false}
 									click={changeCheckBox}
 								/>
 							</div>
@@ -141,42 +138,42 @@ const AddGoods = () => {
 									text={'50 - 80 см'}
 									margin={'0 0 15px 0'}
 									fs={'14px'}
-									checked={logCheckbox}
+									checked={false}
 									click={changeCheckBox}
 								/>
 								<CheckBox
 									text={'80 - 92 см'}
 									margin={'0 0 15px 0'}
 									fs={'14px'}
-									checked={logCheckbox}
+									checked={false}
 									click={changeCheckBox}
 								/>
 								<CheckBox
 									text={'92 - 104 см'}
 									margin={'0 0 15px 0'}
 									fs={'14px'}
-									checked={logCheckbox}
+									checked={false}
 									click={changeCheckBox}
 								/>
 								<CheckBox
 									text={'110 - 122 см'}
 									margin={'0 0 15px 0'}
 									fs={'14px'}
-									checked={logCheckbox}
+									checked={false}
 									click={changeCheckBox}
 								/>
 								<CheckBox
 									text={'128 - 146 см'}
 									margin={'0 0 15px 0'}
 									fs={'14px'}
-									checked={logCheckbox}
+									checked={false}
 									click={changeCheckBox}
 								/>
 								<CheckBox
 									text={'146 - 164 см'}
 									margin={'0 0 15px 0'}
 									fs={'14px'}
-									checked={logCheckbox}
+									checked={false}
 									click={changeCheckBox}
 								/>
 							</div>
@@ -186,7 +183,7 @@ const AddGoods = () => {
 									text={'Демисезон'}
 									margin={'0 0 15px 0'}
 									fs={'14px'}
-									checked={logCheckbox}
+									checked={false}
 									click={changeCheckBox}
 
 								/>
@@ -194,14 +191,14 @@ const AddGoods = () => {
 									text={'Лето'}
 									margin={'0 0 15px 0'}
 									fs={'14px'}
-									checked={logCheckbox}
+									checked={false}
 									click={changeCheckBox}
 								/>
 								<CheckBox
 									text={'Зима'}
 									margin={'0 0 15px 0'}
 									fs={'14px'}
-									checked={logCheckbox}
+									checked={false}
 									click={changeCheckBox}
 								/>
 							</div>
@@ -211,17 +208,17 @@ const AddGoods = () => {
 					<div className="description">
 						<h3 className="description_titile"> Описание</h3>
 						<p className="description_subtitile">* Опишите Вашу вещь: деффекты, особенности использования, и пр</p>
-						<textarea className="description_textarea"></textarea>
+						<textarea className="description_textarea"/>
 					</div>
 					<div className="files">
 						<h3>Загрузите фотографии вашей вещи</h3>
 						<p>Первое фото станет обложкой карточки товара</p>
 						<p>Загружено фотографий 1 из 5</p>
 						<div className = "files_wrapper">
-							<input id="file-input1" className = "file_input" type="file" name="file" multiple></input>
-								<label for="file-input1" className = "files_label added"><span></span></label>
-							<input id="file-input2" className = "file_input " type="file" name="file" multiple></input>
-								<label for="file-input1" className = "files_label "><span></span></label>
+							<input id="file-input1" className = "file_input" type="file" name="file" multiple/>
+								<label htmlFor="file-input1" className = "files_label added"><span/></label>
+							<input id="file-input2" className = "file_input " type="file" name="file" multiple/>
+								<label htmlFor="file-input1" className = "files_label "><span/></label>
 						</div>
 						
 					</div>
