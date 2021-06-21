10f128f979758fbb8a2b0bced72850f321b70857
Roman_Kyrychenko <rp.kyrychenko@gmail.com>
2021-06-21 23:02:59 +0300

    [OBMIN-32] Changed the code for preventing XSS injections and deserialization of untrusted data

--- src/main/java/space/obminyashka/items_exchange/dto/UserLoginDto.java
+++ src/main/java/space/obminyashka/items_exchange/dto/UserLoginDto.java
@@ -1,18 +1,18 @@
 package space.obminyashka.items_exchange.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class UserLoginDto {
 
+    @Size(max = 130, message = "{invalid.max-size}")
     @NotEmpty(message = "{empty.username}")
     private String usernameOrEmail;
 
