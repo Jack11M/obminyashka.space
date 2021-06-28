e0aa2c36fb77eb6b954dc34a76a704c90c1b69be
Wolshebnik <andteslenko@gmail.com>
2021-06-28 23:38:42 +0300

    Obmin 29 popup (#224)
    * OBMIN-29: added popup wrap
    
    * OBMIN-29: reformatted everything
    
    * OBMIN-29: forgot to change link

--- src/main/resources/react/src/REST/Service/networkProvider.js
+++ src/main/resources/react/src/REST/Service/networkProvider.js
@@ -2,15 +2,14 @@ import axios from 'axios';
 import { getStorageUser, getStorageLang } from '../../Utils';
 
 const instance = axios.create({
-  baseURL: 'https://localhost/',
+  baseURL: '/',
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-export const axiosInstance = async (method, url, body = null) => {
-  instance.defaults.headers.common['Authorization'] =
-    'Bearer ' + getStorageUser('user').token;
-  instance.defaults.headers.common['accept-language'] = getStorageLang();
-  return await instance[method](`${url}`, body);
+export const axiosInstance = async ( method, url, body = null ) => {
+	instance.defaults.headers.common['Authorization'] = 'Bearer ' + getStorageUser( 'user' ).token;
+	instance.defaults.headers.common['accept-language'] = getStorageLang();
+	return await instance[method]( `${ url }`, body );
 };
