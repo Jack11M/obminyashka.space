e635c7981e4d370320a1fcb9eee304e1d3934012
Roman Kyrychenko <37712692+rpkyrych@users.noreply.github.com>
2021-11-11 00:59:24 +0200

    No jira code cleanup (#285)
    * [NO_JIRA] Fix table name for Postgres

--- src/main/java/space/obminyashka/items_exchange/dto/RefreshTokenResponseDto.java
+++ src/main/java/space/obminyashka/items_exchange/dto/RefreshTokenResponseDto.java
@@ -5,6 +5,6 @@ import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
 
 public record RefreshTokenResponseDto(@JsonProperty(OAuth2ParameterNames.ACCESS_TOKEN) String accessToken,
                                       @JsonProperty(OAuth2ParameterNames.REFRESH_TOKEN) String refreshToken,
-                                      @JsonProperty("expires_in") String accessTokenExpiration,
+                                      @JsonProperty(OAuth2ParameterNames.EXPIRES_IN) String accessTokenExpiration,
                                       @JsonProperty("refresh_expires_in") String refreshTokenExpiration) {
 }
