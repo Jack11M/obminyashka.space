1646e40b5c84488fe51a0a5cccc4c87508cff11b
Hrytsenko Danylo <cloudmenson9@gmail.com>
2022-07-14 14:44:45 +0300

    [OBMIN-110] Change CSS to Styled-components (file ProductOffers), (merge 107 & 110)

--- src/main/resources/react/src/pages/ProductPage/ProductOffers/styles.js
+++ src/main/resources/react/src/pages/ProductPage/ProductOffers/styles.js
@@ -1,8 +1,10 @@
-.offersBoxes {
+import styled from 'styled-components';
+
+export const OffersContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   margin-top: 50px;
   margin-bottom: 100px;
   width: 1220px;
-}
+`;
