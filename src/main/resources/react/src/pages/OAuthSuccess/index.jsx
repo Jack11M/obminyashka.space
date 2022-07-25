815136096da07f963ed5ade94451afc5492c5d7f
Andrii Teslenko <andrii.teslenko@guardianvets.com>
2022-07-25 22:17:50 +0300

    OBMIN-95 extentions

---
+++ src/main/resources/react/src/pages/OAuthSuccess/index.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { putOauthUserThunk } from 'store/auth/thunk';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+const OAuthSuccess = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const code = location.search.replace('?code=', '');
+
+  const getOAuth2User = async () => {
+    sessionStorage.setItem('code', code);
+    try {
+      await dispatch(putOauthUserThunk());
+      navigate('/');
+    } catch (e) {
+      console.log(e.response);
+    }
+  };
+
+  useEffect(() => {
+    getOAuth2User().then((res) => console.log(res));
+  }, []);
+
+  return null;
+};
+
+export default OAuthSuccess;
