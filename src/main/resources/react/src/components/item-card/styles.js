a7a74d48b79c5f47a7bf2a92036954b63a126879
Danylo Hrytsenko <74321926+cloudmenson@users.noreply.github.com>
2022-07-29 23:17:50 +0300

    [OBMIN-146] Updated CSS to Styles-components (file avatarForCard) and sorted some files (#362)
    * [OBMIN-146] Updated CSS to Styled-components (file avatarForCard) and sorted some files
    
    * OBMIN-146 minor changes
    
    * [OBMIN-146] Updated styles.js
    
    Co-authored-by: Andrii Teslenko <andrii.teslenko@guardianvets.com>

--- src/main/resources/react/src/components/item-card/styles.js
+++ src/main/resources/react/src/components/item-card/styles.js
@@ -1,6 +1,8 @@
 /* eslint-disable indent */
 import styled, { css } from 'styled-components';
 
+import * as Icon from 'assets/icons';
+
 export const Card = styled.div`
   position: relative;
   width: 290px;
@@ -92,25 +94,25 @@ export const ButtonBlock = styled.div`
   display: flex;
 `;
 
-export const InboxSvg = styled.svg`
-  transition: background-color 0.3s ease;
-`;
-
 export const InboxDiv = styled.div`
   position: relative;
   margin-left: 16px;
+  cursor: pointer;
+
+  path {
+    transition: fill 0.3s ease;
+  }
 
   &:hover {
-    cursor: pointer;
-    ${InboxSvg}:hover {
-      cursor: pointer;
+    svg {
       path {
         fill: hsl(116, 60%, 45%);
       }
     }
+  }
 
-    ${InboxSvg}:active {
-      cursor: pointer;
+  &:active {
+    svg {
       path {
         fill: hsl(116, 60%, 41%);
       }
@@ -129,4 +131,22 @@ export const InboxSpan = styled.span`
   font-style: normal;
   font-weight: 800;
   color: #fff;
+`;
+
+export const StylizedAvatar = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 4px;
+  border-radius: 25px;
+  transform: translate(-50%);
+`;
+
+export const FavoriteStar = styled(Icon.FavoriteSarSvg)`
+  display: inline-block;
+  width: 100%;
+  margin: 11px auto;
+
+  path {
+    fill: ${({ isFavorite }) => (isFavorite ? '#12b6ed' : '#fff')};
+  }
 `;
