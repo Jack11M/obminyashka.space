784e3cd05424db6a73f733391cb693e16a5053cc
Andrii Teslenko <andrii.teslenko@guardianvets.com>
2022-08-16 22:39:13 +0300

    [OBMIN-77] fixed helpers

--- src/main/resources/react/src/pages/UserInfo/active_profile/helpers.js
+++ src/main/resources/react/src/pages/UserInfo/active_profile/helpers.js
@@ -1,5 +1,5 @@
 export const getName = (firstName, lastName) => {
   const name = [firstName, lastName];
 
-  return name.filter((item) => item.length > 0).join(' ');
+  return name.filter((item) => item?.length > 0).join(' ');
 };
