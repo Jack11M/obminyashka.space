9234d09b77e479ac1d9183e4eb6f49e53e511d73
Danylo Hrytsenko <74321926+cloudmenson@users.noreply.github.com>
2022-10-03 23:20:51 +0300

    [OBMIN-211] Bug-fixed, renamed_filter_page (#415)

--- src/main/resources/react/src/pages/homepage/HomePage.jsx
+++ src/main/resources/react/src/pages/homepage/HomePage.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { getUserThunk } from 'store/profile/thunk';
 import { useDispatch, useSelector } from 'react-redux';
 
+import { getAuthed } from 'store/auth/slice';
 import { getProfile } from 'store/profile/slice';
 
 import Sliders from './slider';
@@ -13,10 +14,11 @@ import * as Styles from './styles';
 
 const HomePage = () => {
   const dispatch = useDispatch();
+  const isAuthed = useSelector(getAuthed);
   const { avatarImage } = useSelector(getProfile);
 
   useEffect(() => {
-    if (!avatarImage) {
+    if (!avatarImage && isAuthed) {
       dispatch(getUserThunk());
     }
   }, [dispatch]);
