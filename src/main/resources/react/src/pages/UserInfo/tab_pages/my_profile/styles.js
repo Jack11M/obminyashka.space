57fad5d042ae2109d259f088f68fe441a2db97e5
ASGladchenko <105244016+ASGladchenko@users.noreply.github.com>
2023-02-28 23:31:37 +0200

    Obmin 319 create buttons add remove child component in storybook and connect to main project  (#498)
    * [OBMIN-319] up story version, delete AddRemoveChild button swap them for StoryBook Button
    
    * [OBMIN-319] added spacing
    
    * [OBMIN-319] update package-lock.json
    
    * [OBMIN-319] update package.json
    
    * [OBMIN-319] update package-lock.json
    
    * [OBMIN-319] sort, update package-lock.json

---
+++ src/main/resources/react/src/pages/UserInfo/tab_pages/my_profile/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const WrapperAddButton = styled.div`
+  max-width: 150px;
+  margin-left: 135px;
+`;
+
+export const WrapperDelButton = styled.div`
+  position: absolute;
+  top: 4px;
+  right: -40px;
+`;
