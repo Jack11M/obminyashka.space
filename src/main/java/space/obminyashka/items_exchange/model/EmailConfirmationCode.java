a745138faaf324829ba56ec4cf5905472be4b5ee
Serhii <103417930+Sershok@users.noreply.github.com>
2023-04-06 18:07:56 +0300

    [OBMIN-344] rename table and remove column "token"

--- src/main/java/space/obminyashka/items_exchange/model/EmailConfirmationCode.java
+++ src/main/java/space/obminyashka/items_exchange/model/EmailConfirmationCode.java
@@ -10,12 +10,12 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Entity(name = "email_confirmation_token")
+@Entity(name = "email_confirmation_code")
 @Getter
 @Setter
 @NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
-public class EmailConfirmationToken {
+public class EmailConfirmationCode {
     @Id
     @GeneratedValue
     private UUID id;
@@ -24,9 +24,6 @@ public class EmailConfirmationToken {
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
 
-    @Column(nullable = false, unique = true)
-    private String token;
-
     @Column(name = "expiry_date", columnDefinition = "DATE", nullable = false)
     private LocalDateTime expiryDate;
 
@@ -34,9 +31,8 @@ public class EmailConfirmationToken {
     @CreatedDate
     private LocalDateTime created;
 
-    public EmailConfirmationToken(User user, String token, LocalDateTime expiryDate) {
+    public EmailConfirmationCode(User user, LocalDateTime expiryDate) {
         this.user = user;
-        this.token = token;
         this.expiryDate = expiryDate;
     }
 }
