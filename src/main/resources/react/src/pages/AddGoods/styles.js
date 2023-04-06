c42a133754c2c2623af788684e0165345edef211
Voloshkina <dp060990voj@gmail.com>
2023-04-06 21:52:19 +0300

    [OBMIN-349] fixed styles

--- src/main/resources/react/src/pages/AddGoods/styles.js
+++ src/main/resources/react/src/pages/AddGoods/styles.js
@@ -6,28 +6,20 @@ export const MainContainer = styled.main`
 `;
 
 export const Container = styled.div`
-  max-width: 1251px;
   padding: 0 15px;
   margin: auto;
+  max-width: 1251px;
 `;
 
 export const AddContainer = styled.div`
+  padding: 40px;
   min-height: 600px;
   width: 100%;
   background-color: ${({ theme }) => theme.colors.white};
-  padding: 40px;
 `;
 
 export const Wrap = styled.div`
   margin-bottom: 50px;
-`;
-
-export const TitleH3 = styled.h3`
-  font-family: 'Open Sans', sans-serif;
-  font-size: 26px;
-  font-weight: 600;
-  margin-bottom: 10px;
-  line-height: 40px;
 `;
 
 export const WrapItems = styled.div`
@@ -42,67 +34,33 @@ export const SectionsItem = styled.div`
 `;
 
 export const ItemTittle = styled.h4`
+  margin-bottom: 13px;
   font-style: normal;
   font-weight: 400;
   font-size: 22px;
-  margin-bottom: 13px;
   line-height: 27px;
   color: ${(p) => (p.error ? p.theme.colors.colorError : '#383838')};
-`;
-
-export const WrapDescription = styled.div`
-  padding-bottom: 40px;
-`;
-
-export const DescriptionText = styled.p`
-  color: ${({ theme }) => theme.colors.colorGrey};
-  margin-bottom: 10px;
 `;
 
 export const Star = styled.span`
   color: ${({ theme }) => theme.colors.colorRed};
 `;
 
-export const TextArea = styled.textarea`
-  margin-top: 16px;
-  padding: 10px;
-  width: 100%;
-  min-height: 150px;
-  font-size: 16px;
-  line-height: 24px;
-  outline: none;
-  resize: none;
-  border: 1px solid #bcbcbc;
-  border-radius: 2px;
-  caret-color: ${({ theme }) => theme.colors.activeColor};
-
-  &:focus {
-    border-color: hsl(0, 0%, 44%);
-  }
-`;
-
 export const WrapFiles = styled.div`
   margin-bottom: 100px;
 `;
 
-export const FileTittle = styled.h3`
-  font-weight: 600;
-  color: #11171f;
-  font-size: 26px;
-  line-height: 40px;
-`;
-
 export const FileDescription = styled.p`
+  color: ${({ theme }) => theme.colors.borderColor};
   font-size: 16px;
   line-height: 26px;
-  color: #8f8f8f;
   font-weight: 400;
 `;
 
 export const WrapperFile = styled.div`
-  margin-top: 15px;
   display: grid;
   grid-template-columns: repeat(5, 1fr);
+  margin-top: 15px;
 `;
 
 export const WrapButtons = styled.div`
