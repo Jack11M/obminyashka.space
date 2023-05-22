7718209b17bccf7b6edbccca4d7cc3fb27b24e65
Illya Zuew <hd.miner2005@gmail.com>
2023-05-22 19:29:04 +0300

    OBMIN-406 added annotation

--- src/test/java/space/obminyashka/items_exchange/service/AdvertisementServiceTest.java
+++ src/test/java/space/obminyashka/items_exchange/service/AdvertisementServiceTest.java
@@ -1,19 +1,17 @@
 package space.obminyashka.items_exchange.service;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import space.obminyashka.items_exchange.dao.AdvertisementRepository;
 
-import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.*;
-import static space.obminyashka.items_exchange.util.CategoryTestUtil.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static space.obminyashka.items_exchange.util.CategoryTestUtil.NONEXISTENT_ENTITY_ID;
 
 @SpringBootTest
 class AdvertisementServiceTest {
@@ -24,7 +22,7 @@ class AdvertisementServiceTest {
     private AdvertisementService advertisementService;
 
     @ParameterizedTest
-    @MethodSource("getResultsOfInteraction")
+    @CsvSource({"true,true", "false,false"})
     void isAdvertisementsHaveSubcategoryWithId_whenTryToCheckSubcategory_shouldSameResults(boolean returnedValue,
                                                                                            boolean expectedResult) {
         when(advertisementRepository.existsBySubcategoryId(anyLong())).thenReturn(returnedValue);
@@ -32,12 +30,5 @@ class AdvertisementServiceTest {
         final boolean result = advertisementService.areAdvertisementsExistWithSubcategory(NONEXISTENT_ENTITY_ID);
         assertEquals(expectedResult, result);
         verify(advertisementRepository).existsBySubcategoryId(NONEXISTENT_ENTITY_ID);
-    }
-
-    private static Stream<Arguments> getResultsOfInteraction() {
-        return Stream.of(
-                Arguments.of(true, true),
-                Arguments.of(false, false)
-        );
     }
 }
