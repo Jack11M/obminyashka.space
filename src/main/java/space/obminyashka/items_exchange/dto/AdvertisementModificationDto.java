3a391f5809952ead22dd3d354a5035345a80453c
Illya Zuew <hd.miner2005@gmail.com>
2023-06-12 14:20:08 +0300

    OBMIN-399 removed import of useless annotation

--- src/main/java/space/obminyashka/items_exchange/dto/AdvertisementModificationDto.java
+++ src/main/java/space/obminyashka/items_exchange/dto/AdvertisementModificationDto.java
@@ -3,7 +3,6 @@ package space.obminyashka.items_exchange.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
