cb0da1731941e83c2d1a42bfd2d5a9069ca5f99d
Serhii <103417930+Sershok@users.noreply.github.com>
2023-06-19 17:54:41 +0300

    [OBMIN-401] created new exception from case when we get incorrect subcategory id

--- src/main/java/space/obminyashka/items_exchange/controller/AdvertisementController.java
+++ src/main/java/space/obminyashka/items_exchange/controller/AdvertisementController.java
@@ -28,6 +28,7 @@ import space.obminyashka.items_exchange.exception.*;
 import space.obminyashka.items_exchange.exception.bad_request.BadRequestException;
 import space.obminyashka.items_exchange.exception.bad_request.IllegalIdentifierException;
 import space.obminyashka.items_exchange.exception.not_found.CategoryIdNotFoundException;
+import space.obminyashka.items_exchange.exception.not_found.SubcategoryIdNotFoundException;
 import space.obminyashka.items_exchange.model.User;
 import space.obminyashka.items_exchange.service.*;
 import space.obminyashka.items_exchange.util.ResponseMessagesHandler;
@@ -61,7 +62,11 @@ public class AdvertisementController {
             @ApiResponse(responseCode = "200", description = "OK"),
             @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
             @ApiResponse(responseCode = "404", description = "NOT FOUND")})
-    public List<AdvertisementTitleDto> findPaginatedAsThumbnails(@Valid @ParameterObject AdvertisementFindRequest findAdvsRequest) {
+    public List<AdvertisementTitleDto> findPaginatedAsThumbnails(@Valid @RequestBody AdvertisementFindRequest findAdvsRequest) throws SubcategoryIdNotFoundException {
+        if (!subcategoryService.isSubcategoryExistsById(findAdvsRequest.getSubcategoryId())) {
+            throw new SubcategoryIdNotFoundException(getExceptionMessageSourceWithId(findAdvsRequest.getSubcategoryId(),
+                    ResponseMessagesHandler.ValidationMessage.INVALID_SUBCATEGORY_ID));
+        }
         return advertisementService.findThumbnails(findAdvsRequest);
     }
 
