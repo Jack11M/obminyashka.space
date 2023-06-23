b9a07bf842ea488ddf7f7b8a5be76ee3dd84ae5a
Roman Kyrychenko <icroman.kyrychenko@americanwell.com>
2023-06-23 18:41:47 +0300

    [OBMIN-417] Fixed displaying advertisements on the main page and creation of a new advertisement

--- src/main/java/space/obminyashka/items_exchange/model/enums/AgeRange.java
+++ src/main/java/space/obminyashka/items_exchange/model/enums/AgeRange.java
@@ -1,15 +1,18 @@
 package space.obminyashka.items_exchange.model.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Arrays;
 
-import static space.obminyashka.items_exchange.util.MessageSourceUtil.*;
+import static space.obminyashka.items_exchange.util.MessageSourceUtil.getMessageSource;
 import static space.obminyashka.items_exchange.util.ResponseMessagesHandler.ValidationMessage.*;
 
 
+@Slf4j
 @RequiredArgsConstructor
 public enum AgeRange {
     YOUNGER_THAN_1("0-1"),
@@ -24,10 +27,16 @@ public enum AgeRange {
     @Getter
     private final String value;
 
+    @JsonCreator
     public static AgeRange fromValue(String value) {
-        return Arrays.stream(AgeRange.values())
-                .filter(ageRange -> ageRange.value.equals(value))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException(getMessageSource(INVALID_ENUM_VALUE)));
+        try {
+            return AgeRange.valueOf(value);
+        } catch (IllegalArgumentException e) {
+            log.info("Received non-enum value for AgeRange: {value}");
+            return Arrays.stream(AgeRange.values())
+                    .filter(ageRange -> ageRange.value.equals(value))
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalArgumentException(getMessageSource(INVALID_ENUM_VALUE)));
+        }
     }
 }
