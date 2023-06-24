05667b8ffee326ab38b8837732859e65a3fe386e
IllyaZuew <hd.miner2005@gmail.com>
2023-06-24 13:53:14 +0300

    OBMIN-439 wrote tests for UserService

--- src/test/java/space/obminyashka/items_exchange/service/UserServiceTest.java
+++ src/test/java/space/obminyashka/items_exchange/service/UserServiceTest.java
@@ -3,39 +3,47 @@ package space.obminyashka.items_exchange.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.projection.ProjectionFactory;
 import org.springframework.data.projection.SpelAwareProxyProjectionFactory;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import space.obminyashka.items_exchange.dao.EmailConfirmationCodeRepository;
 import space.obminyashka.items_exchange.dao.UserRepository;
+import space.obminyashka.items_exchange.dto.UserLoginResponseDto;
+import space.obminyashka.items_exchange.exception.RefreshTokenException;
 import space.obminyashka.items_exchange.mapper.PhoneMapper;
 import space.obminyashka.items_exchange.mapper.UserMapper;
+import space.obminyashka.items_exchange.model.RefreshToken;
 import space.obminyashka.items_exchange.model.Role;
 import space.obminyashka.items_exchange.model.User;
+import space.obminyashka.items_exchange.model.projection.UserAuthProjection;
 import space.obminyashka.items_exchange.model.projection.UserProjection;
 import space.obminyashka.items_exchange.service.impl.UserServiceImpl;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
-
     public static final String NEW_USER_EMAIL = "user@mail.ua";
-
     @Mock
     private UserRepository userRepository;
     @Mock
@@ -48,6 +56,10 @@ class UserServiceTest {
     private RoleService roleService;
     @Mock
     private UserMapper userMapper;
+    @Captor
+    private ArgumentCaptor<String> usernameCaptor;
+    @Captor
+    private ArgumentCaptor<UserAuthProjection> projectionCaptor;
     private UserServiceImpl userService;
     @Value("${number.of.hours.to.keep.email.confirmation.code}")
     private int numberOfHoursToKeepEmailConformationToken;
@@ -112,5 +124,90 @@ class UserServiceTest {
                 "family_name", "Last"));
 
         return new DefaultOidcUser(Collections.singletonList(new SimpleGrantedAuthority(roleUser)), idToken, userInfo);
+    }
+
+    @Test
+    void findAuthDataByUsernameOrEmail_shouldThrowUsernameNotFoundException() {
+        var expectedUsername = "user";
+        when(userRepository.findAuthDataByEmailOrUsername(any(), any())).thenReturn(Optional.empty());
+
+        assertAll(
+                () -> assertThrows(
+                        UsernameNotFoundException.class,
+                        () -> userService.findAuthDataByUsernameOrEmail(expectedUsername)),
+                () -> verify(userRepository).findAuthDataByEmailOrUsername(usernameCaptor.capture(), any()),
+                () -> assertEquals(expectedUsername, usernameCaptor.getValue()),
+                () -> verify(userMapper, times(0)).toLoginResponseDto(any())
+        );
+    }
+
+    @Test
+    void findAuthDataByUsernameOrEmail_shouldReturnUserLoginResponseDto() {
+        var expectedUsername = "user";
+        var expectedProjection = creatUserAuthProjection();
+        var expectedUserLoginDto = createUserLoginDto(expectedProjection.get());
+        when(userRepository.findAuthDataByEmailOrUsername(any(), any())).thenReturn(expectedProjection);
+        when(userMapper.toLoginResponseDto(any())).thenReturn(expectedUserLoginDto);
+
+        var actualUserLoginDto = userService.findAuthDataByUsernameOrEmail(expectedUsername);
+
+        assertAll(
+                () -> verify(userRepository).findAuthDataByEmailOrUsername(usernameCaptor.capture(), any()),
+                () -> assertEquals(expectedUsername, usernameCaptor.getValue()),
+                () -> verify(userMapper).toLoginResponseDto(projectionCaptor.capture()),
+                () -> assertThat(projectionCaptor.getValue())
+                        .hasFieldOrPropertyWithValue("id", expectedProjection.get().getId())
+                        .hasFieldOrPropertyWithValue("username", expectedProjection.get().getUsername())
+                        .hasFieldOrPropertyWithValue("email", expectedProjection.get().getEmail())
+                        .hasFieldOrPropertyWithValue("firstName", expectedProjection.get().getFirstName())
+                        .hasFieldOrPropertyWithValue("lastName", expectedProjection.get().getLastName())
+                        .hasFieldOrPropertyWithValue("language", expectedProjection.get().getLanguage())
+                        .hasFieldOrPropertyWithValue("role", expectedProjection.get().getRole())
+                        .hasFieldOrPropertyWithValue("refreshToken", expectedProjection.get().getRefreshToken())
+                        .hasFieldOrPropertyWithValue("avatarImage", expectedProjection.get().getAvatarImage()),
+                () -> assertThat(actualUserLoginDto)
+                        .hasFieldOrPropertyWithValue("username", expectedUserLoginDto.getUsername())
+                        .hasFieldOrPropertyWithValue("email", expectedUserLoginDto.getEmail())
+                        .hasFieldOrPropertyWithValue("firstName", expectedUserLoginDto.getFirstName())
+                        .hasFieldOrPropertyWithValue("lastName", expectedUserLoginDto.getLastName())
+                        .hasFieldOrPropertyWithValue("language", expectedUserLoginDto.getLanguage())
+                        .hasFieldOrPropertyWithValue("role", expectedUserLoginDto.getRole())
+                        .hasFieldOrPropertyWithValue("refreshToken", expectedUserLoginDto.getRefreshToken())
+                        .hasFieldOrPropertyWithValue("refreshTokenExpirationDate", expectedUserLoginDto.getRefreshTokenExpirationDate())
+                        .hasFieldOrPropertyWithValue("avatarImage", expectedUserLoginDto.getAvatarImage())
+        );
+    }
+
+    private Optional<UserAuthProjection> creatUserAuthProjection() {
+        ProjectionFactory factory = new SpelAwareProxyProjectionFactory();
+        Role role = new Role();
+        role.setName("ROLE_USER");
+        RefreshToken refreshToken = new RefreshToken("token", LocalDateTime.now());
+        Map<String, Object> map = Map.of(
+                "id", UUID.randomUUID(),
+                "username", "user",
+                "email", "email",
+                "firstName", "name",
+                "lastName", "name",
+                "language", "ua",
+                "role", role,
+                "refreshToken", refreshToken,
+                "avatarImage", new byte[0]
+        );
+        return Optional.ofNullable(factory.createProjection(UserAuthProjection.class, map));
+    }
+
+    private UserLoginResponseDto createUserLoginDto(UserAuthProjection projection) {
+        var userLoginDto = new UserLoginResponseDto();
+        userLoginDto.setUsername(projection.getUsername());
+        userLoginDto.setEmail(projection.getEmail());
+        userLoginDto.setFirstName(projection.getFirstName());
+        userLoginDto.setLastName(projection.getLastName());
+        userLoginDto.setRole(projection.getRole());
+        userLoginDto.setLanguage(projection.getLanguage().toString());
+        userLoginDto.setRefreshToken(projection.getRefreshToken().getToken());
+        userLoginDto.setRefreshTokenExpirationDate(projection.getRefreshToken().getExpiryDate().toString());
+        userLoginDto.setAvatarImage(projection.getAvatarImage());
+        return userLoginDto;
     }
 }
