3ee942a3400ecf61d75b272abd1e67e4d54189cc
ASGladchenko <asgladchenko@gmail.com>
2023-07-19 15:47:26 +0300

    create component

---
+++ frontend/src/components/new_header/config.ts
@@ -0,0 +1,44 @@
+import { getTranslatedText } from '../local';
+import { route } from './../../routes/routeConstants';
+
+export const categories = [
+  {
+    link: route.home,
+    text: getTranslatedText('header.clothes'),
+  },
+  {
+    link: route.home,
+    text: getTranslatedText('header.shoes'),
+  },
+  {
+    link: route.home,
+    text: getTranslatedText('header.toys'),
+  },
+  {
+    link: route.home,
+    text: getTranslatedText('header.vehicles'),
+  },
+  {
+    link: route.home,
+    text: getTranslatedText('header.furniture'),
+  },
+  {
+    link: route.home,
+    text: getTranslatedText('header.babies'),
+  },
+  {
+    link: route.home,
+    text: getTranslatedText('header.books'),
+  },
+  {
+    link: route.home,
+    text: getTranslatedText('header.another'),
+  },
+];
+
+export const burgerLinks = [
+  { to: '/', text: getTranslatedText('header.account') },
+  { to: '/', text: getTranslatedText('button.addAdv'), mobile: true },
+  { to: '/', text: getTranslatedText('header.goodness'), icon: true },
+  { to: '/', text: getTranslatedText('header.about') },
+];
