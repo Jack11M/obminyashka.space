71e9fce5e0090f4b0098ba985ab705c677cfbac4
Serhii <103417930+Sershok@users.noreply.github.com>
2023-07-20 17:54:08 +0300

    [OBMIN-442] formatting

--- src/main/java/space/obminyashka/items_exchange/rest/request/ValidatedEmailRequest.java
+++ src/main/java/space/obminyashka/items_exchange/rest/request/ValidatedEmailRequest.java
@@ -1,12 +1,13 @@
 package space.obminyashka.items_exchange.rest.request;
 
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import space.obminyashka.items_exchange.rest.regexp.PatternHandler;
 
 import static space.obminyashka.items_exchange.rest.response.message.ResponseMessagesHandler.ValidationMessage.*;
 
-public record ValidatedEmailRequest(@Parameter(description = "email for reset password")
+public record ValidatedEmailRequest(@Schema(example = "email@gmail.com",description = "email for reset password")
                                     @Email(regexp = PatternHandler.EMAIL, message = "{" + INVALID_EMAIL + "}")
                                     String email) {
 }
