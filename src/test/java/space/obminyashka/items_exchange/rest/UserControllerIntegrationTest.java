a2d97919f04cffb4c784683679ee7fa7c8cef925
Zuew <hd.miner2005@gmail.com>
2023-08-10 12:31:42 +0300

    OBMIN-470 fixed test

--- src/test/java/space/obminyashka/items_exchange/rest/UserControllerIntegrationTest.java
+++ src/test/java/space/obminyashka/items_exchange/rest/UserControllerIntegrationTest.java
@@ -105,14 +105,14 @@ class UserControllerIntegrationTest extends BasicControllerTest {
         var invalidPhoneAmount = getErrorMessageForInvalidField(INVALID_PHONES_AMOUNT, "{max}", maxPhonesAmount);
         var invalidFirstName = getErrorMessageForInvalidField(INVALID_FIRST_LAST_NAME, "${validatedValue}", userWithInvalidFirstAndLastName.getFirstName());
         var invalidLastName = getErrorMessageForInvalidField(INVALID_FIRST_LAST_NAME, "${validatedValue}", userWithInvalidFirstAndLastName.getLastName());
-        var invalidNotNull = getErrorMessageForInvalidField(INVALID_NOT_NULL_PHONE, "", "");
+        var invalidNotNullPhone = getErrorMessageForInvalidField(INVALID_NOT_NULL_PHONE, "", "");
 
         return Stream.of(
                 Arguments.of(createUserUpdateDtoWithInvalidAmountOfPhones(), invalidPhoneAmount),
                 Arguments.of(createUserUpdateDtoWithInvalidAmountOfPhones(), getMessageSource(INVALID_PHONE_NUMBER)),
                 Arguments.of(userWithInvalidFirstAndLastName, invalidFirstName),
                 Arguments.of(userWithInvalidFirstAndLastName, invalidLastName),
-                Arguments.of(createUserUpdateDtoWithInvalidNullPhone(), invalidNotNull)
+                Arguments.of(createUserUpdateDtoWithInvalidNullPhone(), invalidNotNullPhone)
         );
     }
 
