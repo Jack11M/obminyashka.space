10af35f408067a9ecf2a5d12e52fe2dbe155cae5
Serhii <103417930+Sershok@users.noreply.github.com>
2023-08-15 14:37:11 +0300

    [OBMIN-468] removing unnecessary class

--- src/main/java/space/obminyashka/items_exchange/repository/LocationRepository.java
+++ src/main/java/space/obminyashka/items_exchange/repository/LocationRepository.java
@@ -2,7 +2,6 @@ package space.obminyashka.items_exchange.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import space.obminyashka.items_exchange.repository.model.City;
 import space.obminyashka.items_exchange.repository.model.Location;
 
 import java.util.List;
