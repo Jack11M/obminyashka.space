27749f46c38d7a9a3b2997d58758f51f1445859f
Denys Kyslytsia <kislica779@gmail.com>
2024-04-03 08:58:07 +0300

    [OBMIN-472] Changed config and ENUM

--- frontend/src/components/header/config.ts
+++ frontend/src/components/header/config.ts
@@ -4,35 +4,35 @@ import { getTranslatedText } from "../local";
 
 export const categories = [
   {
-    link: route.home,
+    link: route.SearchResults + "?category=1",
     text: getTranslatedText("header.clothes"),
   },
   {
-    link: route.home,
+    link: route.SearchResults + "?category=2",
     text: getTranslatedText("header.shoes"),
   },
   {
-    link: route.home,
+    link: route.SearchResults + "?category=3",
     text: getTranslatedText("header.toys"),
   },
   {
-    link: route.home,
+    link: route.SearchResults + "?category=4",
     text: getTranslatedText("header.vehicles"),
   },
   {
-    link: route.home,
+    link: route.SearchResults + "?category=5",
     text: getTranslatedText("header.furniture"),
   },
   {
-    link: route.home,
+    link: route.SearchResults + "?category=6",
     text: getTranslatedText("header.babies"),
   },
   {
-    link: route.home,
+    link: route.SearchResults + "?category=7",
     text: getTranslatedText("header.books"),
   },
   {
-    link: route.home,
+    link: route.SearchResults + "?category=8",
     text: getTranslatedText("header.another"),
   },
 ];
