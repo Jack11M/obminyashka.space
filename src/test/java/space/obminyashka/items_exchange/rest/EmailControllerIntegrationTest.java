d2a8614a5283e9add2e868834adf091a3a4d5529
shershok <srh.wrk03@gmail.com>
2024-04-08 13:17:39 +0300

    [OBMIN-512] formatting

--- src/test/java/space/obminyashka/items_exchange/rest/EmailControllerIntegrationTest.java
+++ src/test/java/space/obminyashka/items_exchange/rest/EmailControllerIntegrationTest.java
@@ -3,43 +3,29 @@ package space.obminyashka.items_exchange.rest;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.stubbing.OngoingStubbing;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.SpyBean;
+
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.web.multipart.MultipartFile;
 import space.obminyashka.items_exchange.rest.basic.BasicControllerTest;
-import space.obminyashka.items_exchange.rest.request.ChangeEmailRequest;
 import space.obminyashka.items_exchange.rest.request.ValidationEmailRequest;
 import space.obminyashka.items_exchange.rest.response.message.ResponseMessagesHandler;
-import space.obminyashka.items_exchange.service.MailService;
-import space.obminyashka.items_exchange.service.impl.ImageServiceImpl;
-import space.obminyashka.items_exchange.service.impl.UserServiceImpl;
+
 
 import java.util.Objects;
-import java.util.UUID;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static space.obminyashka.items_exchange.rest.api.ApiKey.EMAIL_RESEND_CODE;
-import static space.obminyashka.items_exchange.rest.api.ApiKey.USER_SERVICE_CHANGE_EMAIL;
 import static space.obminyashka.items_exchange.rest.response.message.MessageSourceProxy.getMessageSource;
-import static space.obminyashka.items_exchange.util.data_producer.UserDtoCreatingUtil.INVALID_EMAIL_WITHOUT_DOMAIN_NAME;
-import static space.obminyashka.items_exchange.util.data_producer.UserDtoCreatingUtil.INVALID_EMAIL_WITHOUT_POINT;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
