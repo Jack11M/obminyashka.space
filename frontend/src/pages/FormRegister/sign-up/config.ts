28c0a1e1298ff80b619173959f50fc3e8ed48abe
Denys Kyslytsia <kislica779@gmail.com>
2024-04-29 15:24:06 +0300

    [OBMIN-362] Added cyrillic validation

--- frontend/src/pages/FormRegister/sign-up/config.ts
+++ frontend/src/pages/FormRegister/sign-up/config.ts
@@ -1,25 +1,37 @@
-import * as Yup from 'yup';
+import * as Yup from "yup";
 
-import { getTranslatedText } from 'src/components/local';
-import { confirmPassword, password } from 'src/Utils/validation';
-import { NO_SPACE, EMAIL_REG_EXP, USERNAME_ALT_CODE_EXP, NUMBER_OF_CHARACTERS } from 'src/config';
+import { getTranslatedText } from "src/components/local";
+import { confirmPassword, password } from "src/Utils/validation";
+import {
+  NO_SPACE,
+  EMAIL_REG_EXP,
+  NUMBER_OF_CHARACTERS,
+  USERNAME_ALT_CODE_EXP,
+} from "src/config";
 
 export const validationSchema = Yup.object().shape({
   email: Yup.string()
-    .required(getTranslatedText('errors.requireField'))
-    .matches(EMAIL_REG_EXP, getTranslatedText('errors.invalidEmailFormat'))
-    .matches(NUMBER_OF_CHARACTERS, getTranslatedText('errors.max129'))
-    .matches(NO_SPACE, getTranslatedText('errors.noSpace'))
-    .default(() => ''),
+    .required(getTranslatedText("errors.requireField"))
+    .matches(EMAIL_REG_EXP, getTranslatedText("errors.invalidEmailFormat"))
+    .test(
+      "no-cyrillic",
+      getTranslatedText("errors.invalidEmailFormat"),
+      (value) => {
+        return !/[а-яА-ЯёЁ]/.test(value || "");
+      }
+    )
+    .matches(NUMBER_OF_CHARACTERS, getTranslatedText("errors.max129"))
+    .matches(NO_SPACE, getTranslatedText("errors.noSpace"))
+    .default(() => ""),
   username: Yup.string()
-    .required(getTranslatedText('errors.requireField'))
-    .min(2, getTranslatedText('errors.min2'))
-    .max(50, getTranslatedText('errors.max50'))
-    .matches(USERNAME_ALT_CODE_EXP, getTranslatedText('errors.altCodeMatch'))
-    .matches(NO_SPACE, getTranslatedText('errors.noSpace'))
-    .default(() => ''),
-  password: password.default(() => ''),
-  confirmPassword: confirmPassword.default(() => ''),
+    .required(getTranslatedText("errors.requireField"))
+    .min(2, getTranslatedText("errors.min2"))
+    .max(50, getTranslatedText("errors.max50"))
+    .matches(USERNAME_ALT_CODE_EXP, getTranslatedText("errors.altCodeMatch"))
+    .matches(NO_SPACE, getTranslatedText("errors.noSpace"))
+    .default(() => ""),
+  password: password.default(() => ""),
+  confirmPassword: confirmPassword.default(() => ""),
   agreement: Yup.boolean()
     .oneOf([true])
     .required()
