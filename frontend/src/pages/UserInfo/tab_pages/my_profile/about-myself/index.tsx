28aabfda5b68a1b08ada5434f722cbd8e9a64773
Denys Kyslytsia <kislica779@gmail.com>
2024-05-01 16:09:54 +0300

    [OBMIN-372] Added react-phone-input-2

--- frontend/src/pages/UserInfo/tab_pages/my_profile/about-myself/index.tsx
+++ frontend/src/pages/UserInfo/tab_pages/my_profile/about-myself/index.tsx
@@ -1,17 +1,20 @@
 /* eslint-disable */
 // @ts-nocheck
 // TODO: fix typescript
-import { useState } from 'react';
-import { FieldArray, Form, Formik } from 'formik';
-import { useDispatch, useSelector } from 'react-redux';
-import { Button, Icon, showMessage, InputField } from 'obminyashka-components';
+import { useState } from "react";
+import PhoneInput from "react-phone-input-2";
+import { FieldArray, Form, Formik } from "formik";
+import { useDispatch, useSelector } from "react-redux";
+import { Button, Icon, showMessage, InputField } from "obminyashka-components";
 
-import api from 'src/REST/Resources';
-import { getProfile, putUserToStore } from 'src/store/profile/slice';
-import { getTranslatedText } from 'src/components/local/localization';
+import api from "src/REST/Resources";
+import { getProfile, putUserToStore } from "src/store/profile/slice";
+import { getTranslatedText } from "src/components/local/localization";
 
-import * as Styles from '../styles';
-import { validationUserSchema, getInitialUser } from './config';
+import { validationUserSchema, getInitialUser } from "./config";
+
+import * as Styles from "../styles";
+import "react-phone-input-2/lib/style.css";
 
 const amount = 3;
 
@@ -22,15 +25,16 @@ const AboutMyself = () => {
 
   const [aboutLoading, setAboutLoading] = useState(false);
 
-  const phoneForInitial = (!phones?.length && ['']) || phones?.map((phone) => phone.phoneNumber);
+  const phoneForInitial =
+    (!phones?.length && [""]) || phones?.map((phone) => phone.phoneNumber);
 
   const transformPhones = (array) =>
     array
       .map((phone) => ({
         defaultPhone: false,
-        phoneNumber: phone.replace(/-|\(|\)/gi, ''),
+        phoneNumber: phone.replace(/-|\(|\)/gi, ""),
       }))
-      .filter((phone) => phone.phoneNumber !== '');
+      .filter((phone) => phone.phoneNumber !== "");
 
   const onSubmit = async (dataFormik) => {
     setAboutLoading(true);
@@ -39,17 +43,23 @@ const AboutMyself = () => {
         ...dataFormik,
         phones: transformPhones(dataFormik.phones),
       };
-      if (!newUserData.lastName && !newUserData.firstName && !newUserData.phones.length) {
-        showMessage.success(getTranslatedText('popup.notEmptyInput'));
+      if (
+        !newUserData.lastName &&
+        !newUserData.firstName &&
+        !newUserData.phones.length
+      ) {
+        showMessage.success(getTranslatedText("popup.notEmptyInput"));
         return;
       }
       await api.profile.putUserInfo(newUserData);
       dispatch(putUserToStore(newUserData));
-      showMessage.success(getTranslatedText('toastText.changedData'));
+      showMessage.success(getTranslatedText("toastText.changedData"));
     } catch (err) {
       if (err.response?.status === 400) {
-        const indexStart = err.response?.data?.error && err.response.data.error.indexOf(':') + 1;
+        const indexStart =
+          err.response?.data?.error && err.response.data.error.indexOf(":") + 1;
         showMessage.error(err.response?.data?.error?.slice(indexStart));
+        console.log(err.response?.data?.error?.slice(indexStart));
       }
     } finally {
       setAboutLoading(false);
@@ -63,33 +73,33 @@ const AboutMyself = () => {
       validationSchema={validationUserSchema}
       initialValues={getInitialUser({ lastName, firstName, phoneForInitial })}
     >
-      {({ values, errors }) => (
+      {({ values, errors, setFieldValue }) => (
         <Form>
           <Styles.WrapperInputWhitOutPhones>
             <InputField
-              type='text'
-              name='firstName'
-              labelColor='black'
-              inputMaxWidth='588px'
-              inputFlexDirection='row'
-              wrapperInputErrorWidth='415px'
-              inputJustifyContent='space-between'
-              label={getTranslatedText('ownInfo.firstName')}
+              type="text"
+              name="firstName"
+              labelColor="black"
+              inputMaxWidth="588px"
+              inputFlexDirection="row"
+              wrapperInputErrorWidth="415px"
+              inputJustifyContent="space-between"
+              label={getTranslatedText("ownInfo.firstName")}
             />
 
             <InputField
-              type='text'
-              name='lastName'
-              labelColor='black'
-              inputMaxWidth='588px'
-              inputFlexDirection='row'
-              wrapperInputErrorWidth='415px'
-              inputJustifyContent='space-between'
-              label={getTranslatedText('ownInfo.lastName')}
+              type="text"
+              name="lastName"
+              labelColor="black"
+              inputMaxWidth="588px"
+              inputFlexDirection="row"
+              wrapperInputErrorWidth="415px"
+              inputJustifyContent="space-between"
+              label={getTranslatedText("ownInfo.lastName")}
             />
           </Styles.WrapperInputWhitOutPhones>
 
-          <FieldArray name='phones'>
+          <FieldArray name="phones">
             {({ push, remove }) => (
               <Styles.WrapperInputPhones>
                 {values.phones?.map((phone, index, arr) => {
@@ -99,17 +109,28 @@ const AboutMyself = () => {
                   const errorField = errors.phones && errors.phones[index];
 
                   return (
-                    <Styles.WrapperInputAddPhones key={String(`phones[${index}]`)}>
-                      <InputField
-                        type='tel'
-                        labelColor='black'
-                        inputMaxWidth='588px'
-                        inputFlexDirection='row'
+                    <Styles.WrapperInputAddPhones
+                      key={String(`phones[${index}]`)}
+                    >
+                      {/* <InputField
+                        type="tel"
+                        labelColor="black"
+                        inputMaxWidth="588px"
+                        inputFlexDirection="row"
                         name={`phones[${index}]`}
-                        wrapperInputErrorWidth='415px'
-                        placeholder='+380(12)345-67-89'
-                        inputJustifyContent='space-between'
-                        label={getTranslatedText('ownInfo.phone')}
+                        wrapperInputErrorWidth="415px"
+                        placeholder="+380(12)345-67-89"
+                        inputJustifyContent="space-between"
+                        label={getTranslatedText("ownInfo.phone")}
+                      /> */}
+
+                      <PhoneInput
+                        country="ua"
+                        name={`phones[${index}]`}
+                        value={values.phones[index]}
+                        onChange={(phone) => {
+                          setFieldValue(`phones[${index}]`, phone);
+                        }}
                       />
 
                       {lastIndex === index && maxArray && (
@@ -119,13 +140,15 @@ const AboutMyself = () => {
                             width={34}
                             height={34}
                             outsideText
-                            colorType='green'
+                            colorType="green"
                             icon={<Icon.Plus />}
-                            text={getTranslatedText('button.addField')}
+                            text={getTranslatedText("button.addField")}
                             onClick={() => {
-                              if (!!phone && !errorField) push('');
+                              if (!!phone && !errorField) push("");
                               else {
-                                showMessage.error(getTranslatedText('ownInfo.choosePhone'));
+                                showMessage.error(
+                                  getTranslatedText("ownInfo.choosePhone")
+                                );
                               }
                             }}
                           />
@@ -139,7 +162,7 @@ const AboutMyself = () => {
                             width={34}
                             height={34}
                             outsideText
-                            colorType='grey'
+                            colorType="grey"
                             icon={<Icon.Plus />}
                             onClick={() => remove(index)}
                           />
@@ -154,11 +177,11 @@ const AboutMyself = () => {
 
           <Button
             width={248}
-            type='submit'
+            type="submit"
             isLoading={aboutLoading}
-            whatClass='btn-form-about-me'
+            whatClass="btn-form-about-me"
             disabling={Object.keys(errors).length}
-            text={getTranslatedText('button.saveChanges')}
+            text={getTranslatedText("button.saveChanges")}
           />
         </Form>
       )}
